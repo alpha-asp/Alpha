6335b249448ef1daa2b8ead10bbdca0620d58515
Lorenz Leutgeb <lorenz.leutgeb@student.tuwien.ac.at>
2016-07-13 00:38:04 +0200

    Add two primitive methods to Grounder interface

--- src/main/java/at/ac/tuwien/kr/alpha/grounder/DummyGrounder.java
+++ src/main/java/at/ac/tuwien/kr/alpha/grounder/DummyGrounder.java
@@ -1,6 +1,10 @@
 package at.ac.tuwien.kr.alpha.grounder;
 
+import at.ac.tuwien.kr.alpha.AnswerSet;
+import at.ac.tuwien.kr.alpha.NoGood;
 import at.ac.tuwien.kr.alpha.grounder.parser.ParsedProgram;
+
+import java.util.Collection;
 
 /**
  * Copyright (c) 2016, the Alpha Team.
@@ -8,5 +12,15 @@ import at.ac.tuwien.kr.alpha.grounder.parser.ParsedProgram;
 public class DummyGrounder extends AbstractGrounder {
 	public DummyGrounder(ParsedProgram program) {
 		super(program);
+	}
+
+	@Override
+	public Collection<NoGood> getNoGoods(int[] ids, boolean[] truthValues) {
+		return null;
+	}
+
+	@Override
+	public AnswerSet translate(int[] trueAtoms) {
+		return null;
 	}
 }
--- src/main/java/at/ac/tuwien/kr/alpha/grounder/Grounder.java
+++ src/main/java/at/ac/tuwien/kr/alpha/grounder/Grounder.java
@@ -1,4 +1,11 @@
 package at.ac.tuwien.kr.alpha.grounder;
 
+import at.ac.tuwien.kr.alpha.AnswerSet;
+import at.ac.tuwien.kr.alpha.NoGood;
+
+import java.util.Collection;
+
 public interface Grounder {
+	Collection<NoGood> getNoGoods(int[] ids, boolean[] truthValues);
+	AnswerSet translate(int[] trueAtoms);
 }
