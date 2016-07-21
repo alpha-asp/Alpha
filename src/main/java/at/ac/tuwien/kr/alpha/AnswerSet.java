0b506d0827c6670a41d045f0223263854a3b5e88
Antonius Weinzierl <antonius.weinzierl@gmail.com>
2016-07-21 13:12:03 +0200

    - Adding methods to the AnswerSet interface.

--- src/main/java/at/ac/tuwien/kr/alpha/AnswerSet.java
+++ src/main/java/at/ac/tuwien/kr/alpha/AnswerSet.java
@@ -1,4 +1,17 @@
 package at.ac.tuwien.kr.alpha;
 
+import at.ac.tuwien.kr.alpha.grounder.GrounderPredicate;
+
+import java.util.List;
+import java.util.Map;
+
 public interface AnswerSet {
+
+	Map<GrounderPredicate, Integer> getPredicateArityMap();
+
+	List<String> getPredicateInstacesAsString(GrounderPredicate predicate, Integer arity);
+
+	List<Integer> getPredicateInstancesAsTermId(GrounderPredicate predicate, Integer arity);
+
+	Map<Integer, String> getTermIdToStringMap();
 }
