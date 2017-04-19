2a90da90a699012a61d9ce1e5e67146b5ffe0276
Richard Taupe <richard.taupe@siemens.com>
2017-04-19 10:33:31 +0200

    Change file encoding to UTF-8 and map characters

--- src/test/java/at/ac/tuwien/kr/alpha/solver/ThreeColouringWheelTest.java
+++ src/test/java/at/ac/tuwien/kr/alpha/solver/ThreeColouringWheelTest.java
@@ -40,7 +40,7 @@ import static at.ac.tuwien.kr.alpha.Main.parseVisit;
 
 /**
  * Tests {@link AbstractSolver} using some three-coloring test cases, as described in:
- * Lef®®vre, Claire; B®¶atrix, Christopher; St®¶phan, Igor; Garcia, Laurent (2017):
+ * Lef√®vre, Claire; B√©atrix, Christopher; St√©phan, Igor; Garcia, Laurent (2017):
  * ASPeRiX, a first-order forward chaining approach for answer set computing.
  * In Theory and Practice of Logic Programming, pp. 1-45.
  * DOI: 10.1017/S1471068416000569
