40cb6ccf1c1e864db13879d82a1509622466fee3
Antonius Weinzierl <antonius.weinzierl@gmail.com>
2017-10-27 12:31:00 +0300

    Adding abstract Graph.

---
+++ src/main/java/at/ac/tuwien/kr/alpha/grounder/structure/Graph.java
@@ -0,0 +1,62 @@
+package at.ac.tuwien.kr.alpha.grounder.structure;
+
+import java.util.*;
+
+/**
+ * Represents a generic graph structure.
+ * Copyright (c) 2017, the Alpha Team.
+ */
+public class Graph<V> {
+	private final HashSet<V> vertices;
+	private final HashSet<AbstractMap.SimpleEntry<V,V>> edges;
+	private final Map<V, Integer> vertexPositions;
+	private int counter;
+
+	public Graph() {
+		vertices = new HashSet<>();
+		edges = new HashSet<>();
+		vertexPositions = new HashMap<>();
+		counter = 0;
+	}
+
+	public void addVertex(V vertex) {
+		if (!vertices.contains(vertex)) {
+			vertices.add(vertex);
+			vertexPositions.put(vertex, counter++);
+		}
+	}
+
+	private void checkAddVertex(V vertex) {
+		if (!vertices.contains(vertex)) {
+			addVertex(vertex);
+		}
+	}
+
+	public void addEdge(V from, V to) {
+		checkAddVertex(from);
+		checkAddVertex(to);
+		edges.add(new AbstractMap.SimpleEntry<V, V>(from, to));
+	}
+
+	public Map<V, Integer> getVertexPositions() {
+		return Collections.unmodifiableMap(vertexPositions);
+	}
+
+	public boolean[][] getAdjacencyMatrix() {
+		boolean[][] matrix = new boolean[vertices.size()][vertices.size()];
+		Map<V, Integer> vertexPositions = getVertexPositions();
+		for (AbstractMap.SimpleEntry<V, V> edge : edges) {
+			matrix[vertexPositions.get(edge.getKey())][vertexPositions.get(edge.getValue())] = true;
+		}
+		return matrix;
+	}
+
+	public LinkedHashMap<V, List<V>> getAdjacencyList() {
+		LinkedHashMap<V, List<V>> list = new LinkedHashMap<>();
+		for (AbstractMap.SimpleEntry<V, V> edge : edges) {
+			list.putIfAbsent(edge.getKey(), new LinkedList<>());
+			list.get(edge.getKey()).add(edge.getValue());
+		}
+		return list;
+	}
+}
