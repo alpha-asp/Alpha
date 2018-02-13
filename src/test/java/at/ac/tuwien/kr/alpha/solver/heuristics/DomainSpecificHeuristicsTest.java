6399d7b8d0b2996bd440d1ed1a61fcf1398f6bbc
Richard Taupe <richard.taupe@siemens.com>
2018-02-13 15:45:53 +0100

    Additional test cases for domain-specific heuristics

--- src/test/java/at/ac/tuwien/kr/alpha/solver/heuristics/DomainSpecificHeuristicsTest.java
+++ src/test/java/at/ac/tuwien/kr/alpha/solver/heuristics/DomainSpecificHeuristicsTest.java
@@ -6,11 +6,11 @@
  * modification, are permitted provided that the following conditions are met:
  * 
  * 1) Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
+ *    list of conditions and the following disclaimer.
  * 
  * 2) Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
  * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -37,8 +37,9 @@ import at.ac.tuwien.kr.alpha.solver.heuristics.BranchingHeuristicFactory.Heurist
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -56,7 +57,7 @@ public class DomainSpecificHeuristicsTest {
 	}
 	
 	@Test
-	public void testSimpleHeuristicProgram_HeuristicAtom_AB_SameLevel() {
+	public void testSimpleGroundHeuristicProgram_HeuristicAtom_AB_SameLevel() {
 		Program program = parser.parse(
 				"a :- not b, not _h(2,1)." + System.lineSeparator() +
 				"b :- not a, not _h(1,1).");
@@ -64,15 +65,33 @@ public class DomainSpecificHeuristicsTest {
 	}
 	
 	@Test
-	public void testSimpleHeuristicProgram_HeuristicAtom_AB_DominatingLevel() {
+	public void testSimpleNonGroundHeuristicProgram_HeuristicAtom_AB_SameLevel() {
+		Program program = parser.parse(
+				"n(1)." + System.lineSeparator() +
+				"a(N) :- n(N), not b(N), not _h(2,1)." + System.lineSeparator() +
+				"b(N) :- n(N), not a(N), not _h(1,1).");
+		solveAndAssertAnswerSets(program, "{ a(1), n(1) }", "{ b(1), n(1) }");
+	}
+	
+	@Test
+	public void testSimpleGroundHeuristicProgram_HeuristicAtom_AB_DominatingLevel() {
 		Program program = parser.parse(
 				"a :- not b, not _h(1,2)." + System.lineSeparator() +
 				"b :- not a, not _h(2,1).");
 		solveAndAssertAnswerSets(program, "{ a }", "{ b }");
 	}
+	
+	@Test
+	public void testSimpleNonGroundHeuristicProgram_HeuristicAtom_AB_DominatingLevel() {
+		Program program = parser.parse(
+				"n(1)." + System.lineSeparator() +
+				"a(N) :- n(N), not b(N), not _h(1,2)." + System.lineSeparator() +
+				"b(N) :- n(N), not a(N), not _h(2,1).");
+		solveAndAssertAnswerSets(program, "{ a(1), n(1) }", "{ b(1), n(1) }");
+	}
 
 	@Test
-	public void testSimpleHeuristicProgram_HeuristicAtom_BA_SameLevel() {
+	public void testSimpleGroundHeuristicProgram_HeuristicAtom_BA_SameLevel() {
 		Program program = parser.parse(
 				"a :- not b, not _h(1,1)." + System.lineSeparator() +
 				"b :- not a, not _h(2,1).");
@@ -80,15 +99,33 @@ public class DomainSpecificHeuristicsTest {
 	}
 
 	@Test
-	public void testSimpleHeuristicProgram_HeuristicAtom_BA_DominatingLevel() {
+	public void testSimpleNonGroundHeuristicProgram_HeuristicAtom_BA_SameLevel() {
+		Program program = parser.parse(
+				"n(1)." + System.lineSeparator() +
+				"a(N) :- n(N), not b(N), not _h(1,1)." + System.lineSeparator() +
+				"b(N) :- n(N), not a(N), not _h(2,1).");
+		solveAndAssertAnswerSets(program, "{ b(1), n(1) }", "{ a(1), n(1) }");
+	}
+
+	@Test
+	public void testSimpleGroundHeuristicProgram_HeuristicAtom_BA_DominatingLevel() {
 		Program program = parser.parse(
 				"a :- not b, not _h(2,1)." + System.lineSeparator() +
 				"b :- not a, not _h(1,2).");
 		solveAndAssertAnswerSets(program, "{ b }", "{ a }");
 	}
+
+	@Test
+	public void testSimpleNonGroundHeuristicProgram_HeuristicAtom_BA_DominatingLevel() {
+		Program program = parser.parse(
+				"n(1)." + System.lineSeparator() +
+				"a(N) :- n(N), not b(N), not _h(2,1)." + System.lineSeparator() +
+				"b(N) :- n(N), not a(N), not _h(1,2).");
+		solveAndAssertAnswerSets(program, "{ b(1), n(1) }", "{ a(1), n(1) }");
+	}
 	
 	@Test
-	public void testSimpleHeuristicProgram_HeuristicAnnotation_AB_SameLevel() {
+	public void testSimpleGroundHeuristicProgram_HeuristicAnnotation_AB_SameLevel() {
 		Program program = parser.parse(
 				"a :- not b. [2@1]" + System.lineSeparator() +
 				"b :- not a. [1@1]");
@@ -96,15 +133,33 @@ public class DomainSpecificHeuristicsTest {
 	}
 	
 	@Test
-	public void testSimpleHeuristicProgram_HeuristicAnnotation_AB_DominatingLevel() {
+	public void testSimpleNonGroundHeuristicProgram_HeuristicAnnotation_AB_SameLevel() {
+		Program program = parser.parse(
+				"n(1)." + System.lineSeparator() +
+				"a(N) :- n(N), not b(N). [2@1]" + System.lineSeparator() +
+				"b(N) :- n(N), not a(N). [1@1]");
+		solveAndAssertAnswerSets(program, "{ a(1), n(1) }", "{ b(1), n(1) }");
+	}
+	
+	@Test
+	public void testSimpleGroundHeuristicProgram_HeuristicAnnotation_AB_DominatingLevel() {
 		Program program = parser.parse(
 				"a :- not b. [1@2]" + System.lineSeparator() +
 				"b :- not a. [2@1]");
 		solveAndAssertAnswerSets(program, "{ a }", "{ b }");
 	}
+	
+	@Test
+	public void testSimpleNonGroundHeuristicProgram_HeuristicAnnotation_AB_DominatingLevel() {
+		Program program = parser.parse(
+				"n(1)." + System.lineSeparator() +
+				"a(N) :- n(N), not b(N). [1@2]" + System.lineSeparator() +
+				"b(N) :- n(N), not a(N). [2@1]");
+		solveAndAssertAnswerSets(program, "{ a(1), n(1) }", "{ b(1), n(1) }");
+	}
 
 	@Test
-	public void testSimpleHeuristicProgram_HeuristicAnnotation_BA_SameLevel() {
+	public void testSimpleGroundHeuristicProgram_HeuristicAnnotation_BA_SameLevel() {
 		Program program = parser.parse(
 				"a :- not b. [1@1]" + System.lineSeparator() +
 				"b :- not a. [2@1]");
@@ -112,7 +167,16 @@ public class DomainSpecificHeuristicsTest {
 	}
 
 	@Test
-	public void testSimpleHeuristicProgram_HeuristicAnnotation_BA_DominatingLevel() {
+	public void testSimpleNonGroundHeuristicProgram_HeuristicAnnotation_BA_SameLevel() {
+		Program program = parser.parse(
+				"n(1)." + System.lineSeparator() +
+				"a(N) :- n(N), not b(N). [1@1]" + System.lineSeparator() +
+				"b(N) :- n(N), not a(N). [2@1]");
+		solveAndAssertAnswerSets(program, "{ b(1), n(1) }", "{ a(1), n(1) }");
+	}
+
+	@Test
+	public void testSimpleGroundHeuristicProgram_HeuristicAnnotation_BA_DominatingLevel() {
 		Program program = parser.parse(
 				"a :- not b. [2@1]" + System.lineSeparator() +
 				"b :- not a. [1@2]");
@@ -120,23 +184,54 @@ public class DomainSpecificHeuristicsTest {
 	}
 
 	@Test
-	public void testSimpleHeuristicProgram_HeuristicAnnotation_SimpleChoiceGenerator_NegativeLiteral() {
+	public void testSimpleNonGroundHeuristicProgram_HeuristicAnnotation_BA_DominatingLevel() {
 		Program program = parser.parse(
-				"c :- not nc. [10@1]" + System.lineSeparator() +
-				"nc :- not c." + System.lineSeparator() +
-				"a :- not b. [1@1 : c]" + System.lineSeparator() +
-				"b :- not a. [1@1 : not c]");
-		solveAndAssertAnswerSets(program, "{ a, c }", "{ b, c }", "{ b, nc }", "{a, nc}");
+				"n(1)." + System.lineSeparator() +
+				"a(N) :- n(N), not b(N). [2@1]" + System.lineSeparator() +
+				"b(N) :- n(N), not a(N). [1@2]");
+		solveAndAssertAnswerSets(program, "{ b(1), n(1) }", "{ a(1), n(1) }");
 	}
 
 	@Test
-	public void testSimpleHeuristicProgram_HeuristicAnnotation_SimpleChoiceGenerator_PositiveLiteral() {
+	public void testSimpleGroundHeuristicProgram_HeuristicAnnotation_SimpleChoiceGenerator_NegativeLiteral() {
 		Program program = parser.parse(
 				"c :- not nc. [10@1]" + System.lineSeparator() +
 				"nc :- not c." + System.lineSeparator() +
 				"a :- not b. [1@1 : c]" + System.lineSeparator() +
 				"b :- not a. [1@1 : not c]");
-		solveAndAssertAnswerSets(program, "{ a, c }", "{ b, c }", "{ b, nc }", "{a, nc}");
+		solveAndAssertAnswerSets(program, "{ a, c }", "{ b, c }", "{ b, nc }", "{ a, nc }");
+	}
+
+	@Test
+	public void testSimpleNonGroundHeuristicProgram_HeuristicAnnotation_SimpleChoiceGenerator_NegativeLiteral() {
+		Program program = parser.parse(
+				"n(1)." + System.lineSeparator() +
+				"c(N) :- n(N), not nc(N). [10@1]" + System.lineSeparator() +
+				"nc(N) :- n(N), not c(N)." + System.lineSeparator() +
+				"a(N) :- n(N), not b(N). [1@1 : c(N)]" + System.lineSeparator() +
+				"b(N) :- n(N), not a(N). [1@1 : not c(N)]");
+		solveAndAssertAnswerSets(program, "{ a(1), c(1), n(1) }", "{ b(1), c(1), n(1) }", "{ b(1), n(1), nc(1) }", "{ a(1), n(1), nc(1) }");
+	}
+
+	@Test
+	public void testSimpleGroundHeuristicProgram_HeuristicAnnotation_SimpleChoiceGenerator_PositiveLiteral() {
+		Program program = parser.parse(
+				"c :- not nc. [10@1]" + System.lineSeparator() +
+				"nc :- not c." + System.lineSeparator() +
+				"a :- not b. [1@1 : c]" + System.lineSeparator() +
+				"b :- not a. [1@1 : not c]");
+		solveAndAssertAnswerSets(program, "{ a, c }", "{ b, c }", "{ b, nc }", "{ a, nc }");
+	}
+
+	@Test
+	public void testSimpleNonGroundHeuristicProgram_HeuristicAnnotation_SimpleChoiceGenerator_PositiveLiteral() {
+		Program program = parser.parse(
+				"n(1)." + System.lineSeparator() +
+				"c(N) :- n(N), not nc(N). [10@1]" + System.lineSeparator() +
+				"nc(N) :- n(N), not c(N)." + System.lineSeparator() +
+				"a(N) :- n(N), not b(N). [1@1 : c(N)]" + System.lineSeparator() +
+				"b(N) :- n(N), not a(N). [1@1 : not c(N)]");
+		solveAndAssertAnswerSets(program, "{ a(1), c(1), n(1) }", "{ b(1), c(1), n(1) }", "{ b(1), n(1), nc(1) }", "{ a(1), n(1), nc(1) }");
 	}
 	
 	@Test
@@ -150,10 +245,6 @@ public class DomainSpecificHeuristicsTest {
 
 	private void solveAndAssertAnswerSets(Program program, String... expectedAnswerSets) {
 		Solver solver = SolverFactory.getInstance("default", "alpharoaming", GrounderFactory.getInstance("naive", program), new Random(), true, Heuristic.NAIVE, true);
-		List<AnswerSet> listOfAnswerSets = solver.collectList();
-		assertEquals(expectedAnswerSets.length, listOfAnswerSets.size());
-		for (int i = 0; i < expectedAnswerSets.length; i++) {
-			assertEquals(expectedAnswerSets[i], listOfAnswerSets.get(i).toString());
-		}
+		assertEquals(Arrays.asList(expectedAnswerSets), solver.stream().map(AnswerSet::toString).collect(Collectors.toList()));
 	}
 }
