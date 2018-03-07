alpha-asp/Alpha

commit 211641771b0f24ac4725eed0ca9e3bd4a0355b71

ng: Simplify code that varied by head index

src/main/java/at/ac/tuwien/kr/alpha/solver/heuristics/DependencyDrivenHeuristic.java
@@ -40,9 +40,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import static at.ac.tuwien.kr.alpha.common.Literals.atomOf;
+import static at.ac.tuwien.kr.alpha.common.Literals.isNegated;
+import static at.ac.tuwien.kr.alpha.common.NoGood.HEAD;
 import static at.ac.tuwien.kr.alpha.solver.Atoms.isAtom;
 import static at.ac.tuwien.kr.alpha.solver.ThriceTruth.FALSE;
 import static at.ac.tuwien.kr.alpha.solver.ThriceTruth.TRUE;
@@ -239,30 +242,23 @@ public class DependencyDrivenHeuristic implements BranchingHeuristic {
 	}
 
 	protected void recordAtomRelationships(NoGood noGood) {
-		if (noGood.isBodyNotHead(choiceManager::isAtomChoice)) {
-			int headIndex = noGood.getHead();
-			int bodyIndex = headIndex != 0 ? 0 : 1;
-			int body = noGood.getAtom(bodyIndex);
-			int head = noGood.getAtom(headIndex);
+		if (isBodyNotHead(noGood, choiceManager::isAtomChoice)) {
+			int body = noGood.getAtom(1);
+			int head = noGood.getAtom(HEAD);
 			bodyToHead.put(body, head);
 			headToBodies.put(head, body);
 			atomsToBodies.put(head, body);
-		} else if (noGood.isBodyElementsNotBody(choiceManager::isAtomChoice)) {
+		} else if (isBodyElementsNotBody(noGood, choiceManager::isAtomChoice)) {
 			Set<Integer> literals = new HashSet<>();
-			int bodyAtom = 0;
+			int bodyAtom = noGood.getAtom(HEAD);
 			for (int i = 0; i < noGood.size(); i++) {
-				if (i == noGood.getHead()) {
-					bodyAtom = noGood.getAtom(i);
-				} else {
-					int literal = noGood.getLiteral(i);
-					literals.add(literal);
-					if (bodyAtom != 0) {
-						atomsToBodies.put(atomOf(literal), bodyAtom);
-					} // else {
-						// TODO
-					// }
-				}
-				// TODO: make more performant (maybe head could always come first in NoGood?)
+				int literal = noGood.getLiteral(i);
+				literals.add(literal);
+				if (bodyAtom != 0) {
+					atomsToBodies.put(atomOf(literal), bodyAtom);
+				} // else {
+					// TODO
+				// }
 			}
 			assert bodyAtom != 0;
 			bodyToLiterals.putAll(bodyAtom, literals);
@@ -333,4 +329,25 @@ public class DependencyDrivenHeuristic implements BranchingHeuristic {
 		return streamOfLiterals.map(Literals::atomOf).max(Comparator.comparingDouble(this::getActivity)).orElse(DEFAULT_CHOICE_ATOM);
 		// TODO: exploit synergy with getMostActiveChoosableAtom
 	}
+
+	/**
+	 * Analyzes the type of this NoGood and checks if it is the so-called "body, not head" type. Uses the given {@code isRuleBody} predicate to check whether an
+	 * atom represents a rule body.
+	 *
+	 * @return {@code true} iff: the NoGood is binary, and it has a head, and its tail is an atom representing a rule body.
+	 */
+	public static boolean isBodyNotHead(NoGood noGood, Predicate<? super Integer> isRuleBody) {
+		return noGood.isBinary() && noGood.hasHead() && isRuleBody.test(atomOf(1));
+	}
+
+	/**
+	 * Analyzes the type of this NoGood and checks if it is the so-called "body elements, not body" type. Uses the given {@code isRuleBody} predicate to check
+	 * whether an atom represents a rule body.
+	 *
+	 * @return {@code true} iff: the NoGood contains at least two literals, and the head is a negative literal whose atom represents a rule body.
+	 */
+	public static boolean isBodyElementsNotBody(NoGood noGood, Predicate<? super Integer> isRuleBody) {
+		return noGood.size() > 1 && noGood.hasHead() && isNegated(noGood.getLiteral(HEAD)) && isRuleBody.test(noGood.getAtom(HEAD));
+	}
+
 }

src/main/java/at/ac/tuwien/kr/alpha/common/AtomTranslator.java
@@ -19,7 +19,12 @@ public interface AtomTranslator {
 	 * @return the string representation of the NoGood.
 	 */
 	default <T extends NoGood> String noGoodToString(T noGood) {
-		StringBuilder sb = new StringBuilder("{");
+		StringBuilder sb = new StringBuilder();
+
+		if (noGood.hasHead()) {
+			sb.append("*");
+		}
+		sb.append("{");
 
 		for (Iterator<Integer> iterator = noGood.iterator(); iterator.hasNext();) {
 			sb.append(literalToString(iterator.next()));
@@ -30,12 +35,6 @@ public interface AtomTranslator {
 		}
 
 		sb.append("}");
-
-		if (noGood.hasHead()) {
-			sb.append("[");
-			sb.append(noGood.getHead());
-			sb.append("]");
-		}
 
 		return sb.toString();
 	}

src/main/java/at/ac/tuwien/kr/alpha/common/NoGood.java
@@ -36,6 +36,7 @@ import static at.ac.tuwien.kr.alpha.common.Literals.atomOf;
 import static at.ac.tuwien.kr.alpha.common.Literals.isNegated;
 
 public class NoGood implements Iterable<Integer>, Comparable<NoGood> {
+	public static final int HEAD = 0;
 	public static final NoGood UNSAT = new NoGood();
 
 	protected final int[] literals;
@@ -63,7 +64,7 @@ public class NoGood implements Iterable<Integer>, Comparable<NoGood> {
 		this.literals = shift <= 0 ? literals : Arrays.copyOf(literals, literals.length - shift);
 	}
 
-	public NoGood(NoGood noGood) {
+	protected NoGood(NoGood noGood) {
 		this.literals = noGood.literals.clone();
 		this.head = noGood.head;
 	}
@@ -88,8 +89,8 @@ public class NoGood implements Iterable<Integer>, Comparable<NoGood> {
 		return literals.length == 2;
 	}
 
-	public int[] getLiteralsClone() {
-		return literals.clone();
+	public NoGood withoutHead() {
+		return new NoGood(literals.clone());
 	}
 
 	/**
@@ -103,44 +104,8 @@ public class NoGood implements Iterable<Integer>, Comparable<NoGood> {
 		return literals[index];
 	}
 
-	/**
-	 * Returns the index of the head literal, if present.
-	 * @throws IllegalStateException if there is no head.
-	 * @return the index of the head literal.
-	 */
-	public int getHead() {
-		return 0;
-	}
-
 	public boolean hasHead() {
 		return head;
-	}
-
-	/**
-	 * Analyzes the type of this NoGood and checks if it is the so-called "body, not head" type. Uses the given {@code isRuleBody} predicate to check whether an
-	 * atom represents a rule body.
-	 * 
-	 * @return {@code true} iff: the NoGood is binary, and it has a head, and its tail is an atom representing a rule body.
-	 */
-	public boolean isBodyNotHead(Predicate<? super Integer> isRuleBody) {
-		return isBinary() && hasHead() && isRuleBody.test(atomOf(getFirstTailLiteral()));
-	}
-
-	/**
-	 * Returns the first literal that is not pointed to by the head.
-	 */
-	private int getFirstTailLiteral() {
-		return head ? literals[1] : literals[0];
-	}
-
-	/**
-	 * Analyzes the type of this NoGood and checks if it is the so-called "body elements, not body" type. Uses the given {@code isRuleBody} predicate to check
-	 * whether an atom represents a rule body.
-	 * 
-	 * @return {@code true} iff: the NoGood contains at least two literals, and the head is a negative literal whose atom represents a rule body.
-	 */
-	public boolean isBodyElementsNotBody(Predicate<? super Integer> isRuleBody) {
-		return literals.length > 1 && hasHead() && isNegated(literals[0]) && isRuleBody.test(atomOf(literals[0]));
 	}
 
 	@Override

src/main/java/at/ac/tuwien/kr/alpha/solver/ArrayAssignment.java
@@ -36,7 +36,6 @@ import org.slf4j.LoggerFactory;
 
 import java.util.*;
 
-import static at.ac.tuwien.kr.alpha.Util.oops;
 import static at.ac.tuwien.kr.alpha.common.Literals.atomOf;
 import static at.ac.tuwien.kr.alpha.solver.Atoms.isAtom;
 import static at.ac.tuwien.kr.alpha.solver.ThriceTruth.*;
@@ -125,7 +124,7 @@ public class ArrayAssignment implements WritableAssignment, Checkable {
 		for (Integer atom : atomsAssignedInDecisionLevel.remove(decisionLevelToRemove)) {
 			Entry current = assignment.get(atom);
 			if (current == null) {
-				throw oops("Entry not in current assignment");
+				throw new RuntimeException("Entry not in current assignment. Should not happen.");
 			}
 			// If assignment was moved to lower decision level, do not remove it while backtracking from previously higher decision level.
 			if (current.getDecisionLevel() < decisionLevelToRemove) {
@@ -188,9 +187,9 @@ public class ArrayAssignment implements WritableAssignment, Checkable {
 	private ConflictCause assignWithDecisionLevel(int atom, ThriceTruth value, NoGood impliedBy, int decisionLevel) {
 		if (checksEnabled) {
 			if (getMBTCount() != getMBTAssignedAtoms().size()) {
-				throw oops("MBT counter and amount of actually MBT-assigned atoms disagree");
+				throw new RuntimeException("MBT counter and amount of actually MBT-assigned atoms disagree. Should not happen.");
 			} else {
-				LOGGER.trace("MBT count agrees with amount of MBT-assigned atoms.");
+				LOGGER.debug("MBT count agrees with amount of MBT-assigned atoms.");
 			}
 		}
 		if (!isAtom(atom)) {
@@ -286,13 +285,13 @@ public class ArrayAssignment implements WritableAssignment, Checkable {
 				}
 				return null;
 		}
-		throw oops("Statement should be unreachable, algorithm misses some case");
+		throw new RuntimeException("Statement should be unreachable, algorithm misses some case.");
 	}
 
 	private void recordMBTBelowTrue(int atom, ThriceTruth value, NoGood impliedBy, int decisionLevel) {
 		Entry oldEntry = get(atom);
 		if (!TRUE.equals(oldEntry.getTruth()) || !MBT.equals(value)) {
-			throw oops("Recording MBT below TRUE but truth values do not match");
+			throw new RuntimeException("Recording MBT below TRUE but truth values do not match. Should not happen.");
 		}
 		//final int previousPropagationLevel = atomsAssignedInDecisionLevel.get(decisionLevel).size();
 		final int previousPropagationLevel = propagationCounterPerDecisionLevel.get(decisionLevel);
@@ -317,10 +316,10 @@ public class ArrayAssignment implements WritableAssignment, Checkable {
 	private void recordAssignment(int atom, ThriceTruth value, NoGood impliedBy, int decisionLevel, Entry previous) {
 		Entry oldEntry = get(atom);
 		if (oldEntry != null && decisionLevel >= oldEntry.getDecisionLevel() && !(TRUE.equals(value) && MBT.equals(oldEntry.getTruth()))) {
-			throw oops("Assigning value into higher decision level");
+			throw new RuntimeException("Assigning value into higher decision level. Should not happen.");
 		}
 		if (previous != null && (!TRUE.equals(value) || !MBT.equals(previous.getTruth()))) {
-			throw oops("Assignment has previous value, but truth values are not MBT (previously) and TRUE (now)");
+			throw new RuntimeException("Assignment has previous value, but truth values are not MBT (previously) and TRUE (now). Should not happen.");
 		}
 		// Create and record new assignment entry.
 		final int propagationLevel = propagationCounterPerDecisionLevel.get(decisionLevel);
@@ -456,7 +455,7 @@ public class ArrayAssignment implements WritableAssignment, Checkable {
 			this.atom = atom;
 			this.isReassignAtLowerDecisionLevel = isReassignAtLowerDecisionLevel;
 			if (previous != null && !(value == TRUE && previous.value == MBT)) {
-				throw oops("Assignment.Entry instantiated with previous entry set and truth values other than TRUE now and MBT previous");
+				throw new RuntimeException("Assignment.Entry instantiated with previous entry set and truth values other than TRUE now and MBT previous. Should not happen.");
 			}
 		}
 

src/main/java/at/ac/tuwien/kr/alpha/solver/NaiveNoGoodStore.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 
 import static at.ac.tuwien.kr.alpha.common.Literals.*;
+import static at.ac.tuwien.kr.alpha.common.NoGood.HEAD;
 import static at.ac.tuwien.kr.alpha.solver.ThriceTruth.*;
 
 public class NaiveNoGoodStore implements NoGoodStore {
@@ -154,7 +155,7 @@ public class NaiveNoGoodStore implements NoGoodStore {
 			return null;
 		}
 
-		final int head = noGood.getAtom(noGood.getHead());
+		final int head = noGood.getAtom(HEAD);
 
 		if (assignment.getTruth(head) != MBT) {
 			return null;
@@ -164,11 +165,7 @@ public class NaiveNoGoodStore implements NoGoodStore {
 		// (i.e., without the head set it would be unit) and
 		// that none of the true values are assigned MBT, but
 		// instead are all TRUE.
-		for (int i = 0; i < noGood.size(); i++) {
-			if (noGood.getHead() == i) {
-				continue;
-			}
-
+		for (int i = 1; i < noGood.size(); i++) {
 			final int literal = noGood.getLiteral(i);
 
 			if (!assignment.isViolated(literal)) {

src/main/java/at/ac/tuwien/kr/alpha/solver/NaiveSolver.java
@@ -39,6 +39,7 @@ import java.util.*;
 import java.util.function.Consumer;
 
 import static at.ac.tuwien.kr.alpha.common.Literals.*;
+import static at.ac.tuwien.kr.alpha.common.NoGood.HEAD;
 import static at.ac.tuwien.kr.alpha.solver.ThriceTruth.FALSE;
 import static at.ac.tuwien.kr.alpha.solver.ThriceTruth.TRUE;
 import static java.lang.Math.abs;
@@ -425,7 +426,7 @@ public class NaiveSolver extends AbstractSolver {
 			return false;
 		}
 
-		int headAtom = noGood.getAtom(noGood.getHead());
+		int headAtom = noGood.getAtom(HEAD);
 
 		// Check whether head is assigned MBT.
 		if (!mbtAssigned.contains(headAtom)) {
@@ -434,10 +435,7 @@ public class NaiveSolver extends AbstractSolver {
 
 		// Check that NoGood is violated except for the head (i.e., without the head set it would be unit)
 		// and that none of the true values is MBT.
-		for (int i = 0; i < noGood.size(); i++) {
-			if (noGood.getHead() == i) {
-				continue;
-			}
+		for (int i = 1; i < noGood.size(); i++) {
 			int literal = noGood.getLiteral(i);
 			if (!(isLiteralAssigned(literal) && isLiteralViolated(literal))) {
 				return false;

src/main/java/at/ac/tuwien/kr/alpha/solver/NoGoodStoreAlphaRoaming.java
@@ -36,6 +36,7 @@ import java.util.*;
 
 import static at.ac.tuwien.kr.alpha.Util.oops;
 import static at.ac.tuwien.kr.alpha.common.Literals.*;
+import static at.ac.tuwien.kr.alpha.common.NoGood.HEAD;
 import static at.ac.tuwien.kr.alpha.solver.ThriceTruth.*;
 
 /**
@@ -125,7 +126,7 @@ public class NoGoodStoreAlphaRoaming implements NoGoodStore, Checkable {
 	 */
 	private ConflictCause addUnary(final NoGood noGood) {
 		if (noGood.hasHead()) {
-			return assignStrongComplement(0, noGood, 0);
+			return assignStrongComplement(noGood, 0);
 		} else {
 			return assignWeakComplement(0, noGood, 0);
 		}
@@ -173,7 +174,7 @@ public class NoGoodStoreAlphaRoaming implements NoGoodStore, Checkable {
 				}
 			}
 			// Check strong unassigned for nogoods with head.
-			if (noGood.hasHead() && noGood.getHead() != i && (literalEntry == null || literalEntry.getTruth().isMBT())) {
+			if (noGood.hasHead() && i != 0 && (literalEntry == null || literalEntry.getTruth().isMBT())) {
 				posStrongUnassigned = i;
 			}
 			// Check satisfaction
@@ -244,15 +245,15 @@ public class NoGoodStoreAlphaRoaming implements NoGoodStore, Checkable {
 				wng.setAlphaPointer(posStrongUnassigned);
 			} else if (posSatisfiedLiteral1 == -1 && !containsPositiveAssignedMBT) {
 				// Strongly unit.
-				ConflictCause conflictCause = assignStrongComplement(noGood.getHead(), noGood, strongDecisionLevelHighestAssigned);
+				ConflictCause conflictCause = assignStrongComplement(noGood, strongDecisionLevelHighestAssigned);
 				if (conflictCause != null) {
 					return conflictCause;
 				}
 				wng.setAlphaPointer(posStrongHighestAssigned);
-			} else if (posSatisfiedLiteral1 == noGood.getHead() && posSatisfiedLiteral2 == -1 && posStrongUnassigned == -1) {
+			} else if (posSatisfiedLiteral1 == 0 && posSatisfiedLiteral2 == -1 && posStrongUnassigned == -1) {
 				// The head is the only satisfying literal, hence it might propagate on lower decision level.
 				if (satisfiedLiteralEntry.getStrongDecisionLevel() > strongDecisionLevelHighestAssigned) {
-					ConflictCause conflictCause = assignStrongComplement(posSatisfiedLiteral1, noGood, strongDecisionLevelHighestAssigned);
+					ConflictCause conflictCause = assignStrongComplement(noGood, strongDecisionLevelHighestAssigned);
 					if (conflictCause != null) {
 						return conflictCause;
 					}
@@ -306,8 +307,8 @@ public class NoGoodStoreAlphaRoaming implements NoGoodStore, Checkable {
 				return conflictCause;
 			}
 			// Propagate strongly if applicable.
-			if (noGood.hasHead() && noGood.getHead() == propagateePos && MBT != violatedEntry.getTruth()) {
-				conflictCause = assignStrongComplement(propagateePos, noGood, violatedEntry.getStrongDecisionLevel());
+			if (noGood.hasHead() && 0 == propagateePos && MBT != violatedEntry.getTruth()) {
+				conflictCause = assignStrongComplement(noGood, violatedEntry.getStrongDecisionLevel());
 				if (conflictCause != null) {
 					return conflictCause;
 				}
@@ -324,11 +325,10 @@ public class NoGoodStoreAlphaRoaming implements NoGoodStore, Checkable {
 		// might propagate TRUE (and not only FALSE or MBT, which
 		// are accounted for above).
 		if (noGood.hasHead()) {
-			final int head = noGood.getHead();
-			final int bodyLiteral = noGood.getLiteral(head == 0 ? 1 : 0);
+			final int bodyLiteral = noGood.getLiteral(1);
 
 			// Set up watch.
-			watches(bodyLiteral).binary.getAlpha(isPositive(bodyLiteral)).add(new BinaryWatch(noGood, head));
+			watches(bodyLiteral).binary.getAlpha(isPositive(bodyLiteral)).add(new BinaryWatch(noGood, 0));
 		}
 		return null;
 	}
@@ -339,8 +339,8 @@ public class NoGoodStoreAlphaRoaming implements NoGoodStore, Checkable {
 		return assignTruth(atomOf(literal), truth, impliedBy, decisionLevel);
 	}
 
-	private ConflictCause assignStrongComplement(final int literalIndex, final NoGood impliedBy, int decisionLevel) {
-		final int literal = impliedBy.getLiteral(literalIndex);
+	private ConflictCause assignStrongComplement(final NoGood impliedBy, int decisionLevel) {
+		final int literal = impliedBy.getLiteral(0);
 		ThriceTruth truth = isNegated(literal) ? TRUE : FALSE;
 		return assignTruth(atomOf(literal), truth, impliedBy, decisionLevel);
 	}
@@ -463,7 +463,11 @@ public class NoGoodStoreAlphaRoaming implements NoGoodStore, Checkable {
 		// Process binary watches.
 		Watches<BinaryWatch, WatchedNoGood> watchesOfAssignedAtom = watches(entry.getAtom());
 		for (BinaryWatch binaryWatch : watchesOfAssignedAtom.binary.getAlpha(entry.getTruth().toBoolean())) {
-			ConflictCause conflictCause = assignStrongComplement(binaryWatch.getOtherLiteralIndex(), binaryWatch.getNoGood(), entry.getDecisionLevel());
+			if (binaryWatch.getOtherLiteralIndex() != 0) {
+				throw oops("Binary watch for nogood with head does not point at head");
+			}
+
+			ConflictCause conflictCause = assignStrongComplement(binaryWatch.getNoGood(), entry.getDecisionLevel());
 			if (conflictCause != null) {
 				return conflictCause;
 			}
@@ -480,12 +484,11 @@ public class NoGoodStoreAlphaRoaming implements NoGoodStore, Checkable {
 			}
 
 			final int assignedIndex = watchedNoGood.getAlphaPointer();
-			final int headIndex = watchedNoGood.getHead();
 
 			boolean isNoGoodSatisfiedByHead = false;
-			Assignment.Entry headEntry = assignment.get(atomOf(watchedNoGood.getLiteral(headIndex)));
+			Assignment.Entry headEntry = assignment.get(watchedNoGood.getAtom(HEAD));
 			// Check if the other watch already satisfies the noGood.
-			if (headEntry != null && TRUE.equals(headEntry.getTruth()) && !isPositive(watchedNoGood.getLiteral(headIndex))) {
+			if (headEntry != null && TRUE.equals(headEntry.getTruth()) && !isPositive(watchedNoGood.getLiteral(0))) {
 				isNoGoodSatisfiedByHead = true;
 			}
 
@@ -494,8 +497,8 @@ public class NoGoodStoreAlphaRoaming implements NoGoodStore, Checkable {
 			boolean foundPointerCandidate = false;
 
 			// Find new literal to watch.
-			for (int i = 0; i < watchedNoGood.size(); i++) {
-				if (i == assignedIndex || i == headIndex) {
+			for (int i = 1; i < watchedNoGood.size(); i++) {
+				if (i == assignedIndex) {
 					continue;
 				}
 				int currentLiteral = watchedNoGood.getLiteral(i);
@@ -529,7 +532,7 @@ public class NoGoodStoreAlphaRoaming implements NoGoodStore, Checkable {
 			}
 
 			// NoGood is unit, propagate (on potentially lower decision level).
-			ConflictCause conflictCause = assignStrongComplement(headIndex, watchedNoGood, highestDecisionLevel);
+			ConflictCause conflictCause = assignStrongComplement(watchedNoGood, highestDecisionLevel);
 			if (conflictCause != null) {
 				return conflictCause;
 			}
@@ -684,7 +687,7 @@ public class NoGoodStoreAlphaRoaming implements NoGoodStore, Checkable {
 				throw oops("Watch invariant (alpha) violated");
 			}
 			for (WatchedNoGood watchedNoGood : watches.multary.getAlpha(truth)) {
-				int headLiteral = watchedNoGood.getLiteral(watchedNoGood.getHead());
+				int headLiteral = watchedNoGood.getLiteral(0);
 				Assignment.Entry headEntry = assignment.get(atomOf(headLiteral));
 				boolean headViolates = headEntry != null && isPositive(headLiteral) == headEntry.getTruth().toBoolean();
 				int headDecisionLevel = strongDecisionLevel(headEntry);

src/main/java/at/ac/tuwien/kr/alpha/solver/WatchedNoGood.java
@@ -2,6 +2,8 @@ package at.ac.tuwien.kr.alpha.solver;
 
 import at.ac.tuwien.kr.alpha.common.NoGood;
 
+import java.util.Arrays;
+
 public final class WatchedNoGood extends NoGood {
 	private int a;
 	private int b;

commit 2a2cb1cc68c72e736851543a774e33ea3f87f2f9

Fix bug in DependencyDrivenHeuristic
Bug has been introduced by 211641771b0f24ac4725eed0ca9e3bd4a0355b71
and made DependencyDrivenHeuristic fail to recognize "body-not-head"
nogoods

src/main/java/at/ac/tuwien/kr/alpha/solver/heuristics/DependencyDrivenHeuristic.java
@@ -337,7 +337,7 @@ public class DependencyDrivenHeuristic implements BranchingHeuristic {
 	 * @return {@code true} iff: the NoGood is binary, and it has a head, and its tail is an atom representing a rule body.
 	 */
 	public static boolean isBodyNotHead(NoGood noGood, Predicate<? super Integer> isRuleBody) {
-		return noGood.isBinary() && noGood.hasHead() && isRuleBody.test(atomOf(1));
+		return noGood.isBinary() && noGood.hasHead() && isRuleBody.test(noGood.getAtom(1));
 	}
 
 	/**