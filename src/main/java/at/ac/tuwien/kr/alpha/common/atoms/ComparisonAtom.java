40e7bc1903d277b7fa11a2e4b2941878cdd12299
Richard Taupe <rtaupe@edu.aau.at>
2018-05-10 17:28:05 +0200

    Cosmetic changes to reduce differences to master branch.

--- src/main/java/at/ac/tuwien/kr/alpha/common/atoms/ComparisonAtom.java
+++ src/main/java/at/ac/tuwien/kr/alpha/common/atoms/ComparisonAtom.java
@@ -3,7 +3,7 @@
  * All rights reserved.
  * 
  * Additional changes made by Siemens.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * 
