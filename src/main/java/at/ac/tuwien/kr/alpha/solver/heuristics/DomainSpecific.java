alpha-asp/Alpha

commit 0bfa46a1d5b3a15f6e5ca9bb05daba3baa2c6771

Better performance in choosing methods of heuristics
... more specifically, in NaiveHeuristic#chooseAtom(Set<Integer>) and
DomainSpecific#chooseLiteral(Set<Integer>)

src/main/java/at/ac/tuwien/kr/alpha/solver/heuristics/DomainSpecific.java
@@ -92,16 +92,16 @@ public class DomainSpecific implements BranchingHeuristic {
 
 	@Override
 	public int chooseLiteral(Set<Integer> admissibleChoices) {
-		// TODO: improve performance of this method (and called methods)
 		Set<Integer> activeHeuristics = choiceManager.getAllActiveHeuristicAtoms();
 		Collection<Set<Integer>> heuristicsOrderedByDecreasingPriority = choiceManager.getDomainSpecificHeuristics()
 				.getHeuristicsOrderedByDecreasingPriority();
 		for (Set<Integer> potentialHeuristics : heuristicsOrderedByDecreasingPriority) {
 			Map<HeuristicDirectiveValues, Set<Integer>> activeHeuristicsToBodies = new HashMap<>();
+			potentialHeuristics.retainAll(activeHeuristics);
 			for (int h : potentialHeuristics) {
 				HeuristicDirectiveValues values = choiceManager.getDomainSpecificHeuristics().getValues(h);
 				int headAtomId = values.getHeadAtomId();
-				if (activeHeuristics.contains(h) && isUnassigned(headAtomId)) {
+				if (isUnassignedOrMBT(headAtomId)) {
 					Set<Integer> activeChoiceAtomsDerivingHead = choiceManager.getActiveChoiceAtomsDerivingHead(headAtomId);
 					if (!activeChoiceAtomsDerivingHead.isEmpty()) {
 						activeHeuristicsToBodies.put(values, activeChoiceAtomsDerivingHead);
@@ -151,7 +151,7 @@ public class DomainSpecific implements BranchingHeuristic {
 		return Literals.fromAtomAndSign(atom, sign);
 	}
 
-	protected boolean isUnassigned(int atom) {
+	protected boolean isUnassignedOrMBT(int atom) {
 		ThriceTruth truth = assignment.getTruth(atom);
 		return truth != FALSE && truth != TRUE; // do not use assignment.isAssigned(atom) because we may also choose MBTs
 	}

src/main/java/at/ac/tuwien/kr/alpha/solver/heuristics/NaiveHeuristic.java
@@ -30,8 +30,6 @@ package at.ac.tuwien.kr.alpha.solver.heuristics;
 import at.ac.tuwien.kr.alpha.common.NoGood;
 import at.ac.tuwien.kr.alpha.solver.ChoiceManager;
 import at.ac.tuwien.kr.alpha.solver.learning.GroundConflictNoGoodLearner.ConflictAnalysisResult;
-import org.apache.commons.collections4.SetUtils;
-import org.apache.commons.collections4.SetUtils.SetView;
 
 import java.util.Collection;
 import java.util.Set;
@@ -74,12 +72,14 @@ public class NaiveHeuristic implements BranchingHeuristic {
 		if (admissibleChoices == null) {
 			return chooseLiteral();
 		}
-		SetView<Integer> admissibleActiveChoices = SetUtils.intersection(choiceManager.getAllActiveChoiceAtoms(), admissibleChoices);
-		if (admissibleActiveChoices.isEmpty()) {
-			return DEFAULT_CHOICE_LITERAL;
-		} else {
-			return admissibleActiveChoices.iterator().next();
+
+		for (Integer atom : choiceManager.getAllActiveChoiceAtoms()) {
+			if (admissibleChoices.contains(atom)) {
+				return atom;
+			}
 		}
+		
+		return DEFAULT_CHOICE_ATOM;
 	}
 
 	@Override

src/main/java/at/ac/tuwien/kr/alpha/solver/heuristics/domspec/DefaultDomainSpecificHeuristicsStore.java
@@ -73,10 +73,20 @@ public class DefaultDomainSpecificHeuristicsStore implements DomainSpecificHeuri
 
 	@Override
 	public Collection<Set<Integer>> getHeuristicsOrderedByDecreasingPriority() {
-		Stream<Set<Integer>> flatMap = mapLevelWeightHeuristics.values().stream().flatMap(m -> m.values().stream());
+		Stream<Set<Integer>> flatMap = streamHeuristicsOrderedByDecreasingPriority();
 		// do not return flatMap directly because of Java bug
 		// cf. https://stackoverflow.com/questions/29229373/why-filter-after-flatmap-is-not-completely-lazy-in-java-streams
 		return flatMap.collect(Collectors.toList());
+	}
+
+	/**
+	 * Note: we have already tried to use this method instead of getHeuristicsOrderedByDecreasingPriority
+	 * in at.ac.tuwien.kr.alpha.solver.heuristics.DomainSpecific.chooseLiteral(Set<Integer>),
+	 * but it did not yield performance benefits
+	 */
+	@Override
+	public Stream<Set<Integer>> streamHeuristicsOrderedByDecreasingPriority() {
+		return mapLevelWeightHeuristics.values().stream().flatMap(m -> m.values().stream());
 	}
 	
 	@Override

src/main/java/at/ac/tuwien/kr/alpha/solver/heuristics/domspec/DomainSpecificHeuristicsStore.java
@@ -29,6 +29,7 @@ import at.ac.tuwien.kr.alpha.common.heuristics.HeuristicDirectiveValues;
 
 import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Stream;
 
 /**
  * Stores information on heuristic directives obtained from the grounder
@@ -43,6 +44,8 @@ public interface DomainSpecificHeuristicsStore {
 	 * second-to-highest weight, etc.
 	 */
 	Collection<Set<Integer>> getHeuristicsOrderedByDecreasingPriority();
+	
+	Stream<Set<Integer>> streamHeuristicsOrderedByDecreasingPriority();
 	
 	Set<Integer> getAllEntries();
 	

src/main/java/at/ac/tuwien/kr/alpha/solver/heuristics/domspec/EmptyDomainSpecificHeuristicsStore.java
@@ -30,6 +30,7 @@ import at.ac.tuwien.kr.alpha.common.heuristics.HeuristicDirectiveValues;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
+import java.util.stream.Stream;
 
 /**
  * Does nothing (to be used instead of {@link DefaultDomainSpecificHeuristicsStore} in case the domain-specific heuristics are not used)
@@ -43,6 +44,11 @@ public class EmptyDomainSpecificHeuristicsStore implements DomainSpecificHeurist
 	@Override
 	public Collection<Set<Integer>> getHeuristicsOrderedByDecreasingPriority() {
 		return Collections.emptyList();
+	}
+	
+	@Override
+	public Stream<Set<Integer>> streamHeuristicsOrderedByDecreasingPriority() {
+		return Stream.empty();
 	}
 	
 	@Override

commit abebcf621944d8a4a79d05d6b984d065d0303e65

fix bug which caused incorrect domain-specific heuristic choices
bug was introduced in 0bfa46a1d5b3a15f6e5ca9bb05daba3baa2c6771

src/main/java/at/ac/tuwien/kr/alpha/solver/heuristics/DomainSpecific.java
@@ -33,6 +33,7 @@ import at.ac.tuwien.kr.alpha.common.heuristics.HeuristicDirectiveValues;
 import at.ac.tuwien.kr.alpha.solver.ChoiceManager;
 import at.ac.tuwien.kr.alpha.solver.ThriceTruth;
 import at.ac.tuwien.kr.alpha.solver.learning.GroundConflictNoGoodLearner.ConflictAnalysisResult;
+import org.apache.commons.collections4.SetUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -115,7 +116,7 @@ public class DomainSpecific implements BranchingHeuristic {
 
 	private Map<HeuristicDirectiveValues, Set<Integer>> computeActiveHeuristicsToBodies(Set<Integer> activeHeuristics, Set<Integer> potentialHeuristics) {
 		Map<HeuristicDirectiveValues, Set<Integer>> activeHeuristicsToBodies = new HashMap<>();
-		potentialHeuristics.retainAll(activeHeuristics);
+		potentialHeuristics = SetUtils.intersection(potentialHeuristics, activeHeuristics);
 		for (int h : potentialHeuristics) {
 			HeuristicDirectiveValues values = choiceManager.getDomainSpecificHeuristics().getValues(h);
 			int headAtomId = values.getHeadAtomId();