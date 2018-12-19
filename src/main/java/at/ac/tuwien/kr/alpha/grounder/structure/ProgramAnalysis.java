4827f0ced25219a8af1ae4355487c93f0bc2e5db
Michael Langowski <e1426581@student.tuwien.ac.at>
2018-12-19 03:48:29 +0100

    consolidated initialization code in NaiveGrounder, wrapped a few things in ProgramAnalysis in order to make DependencyGraph easier to test

--- src/main/java/at/ac/tuwien/kr/alpha/grounder/structure/ProgramAnalysis.java
+++ src/main/java/at/ac/tuwien/kr/alpha/grounder/structure/ProgramAnalysis.java
@@ -1,31 +1,74 @@
 package at.ac.tuwien.kr.alpha.grounder.structure;
 
-import at.ac.tuwien.kr.alpha.common.Predicate;
-import at.ac.tuwien.kr.alpha.common.Program;
-import at.ac.tuwien.kr.alpha.grounder.NonGroundRule;
-
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
+
+import at.ac.tuwien.kr.alpha.common.Predicate;
+import at.ac.tuwien.kr.alpha.common.Program;
+import at.ac.tuwien.kr.alpha.common.Rule;
+import at.ac.tuwien.kr.alpha.common.atoms.Atom;
+import at.ac.tuwien.kr.alpha.grounder.FactIntervalEvaluator;
+import at.ac.tuwien.kr.alpha.grounder.Instance;
+import at.ac.tuwien.kr.alpha.grounder.NonGroundRule;
 
 /**
  * Copyright (c) 2017, the Alpha Team.
  */
 public class ProgramAnalysis {
 
-	private final Map<Predicate, HashSet<NonGroundRule>> predicateDefiningRules;
+	private final Map<Predicate, HashSet<NonGroundRule>> predicateDefiningRules = new HashMap<>();
+	private final Map<Predicate, LinkedHashSet<Instance>> programFacts = new LinkedHashMap<>();
+	private final Map<Integer, NonGroundRule> nonGroundRules = new HashMap<>();
 
 	public ProgramAnalysis(Program program) {
-		predicateDefiningRules = new HashMap<>();
+		this.initializeFacts(program);
+		this.initializeRules(program);
+	}
+
+	private void initializeFacts(Program program) {
+		List<Instance> tmpInstances;
+		Predicate tmpPredicate;
+		for (Atom fact : program.getFacts()) {
+			tmpInstances = FactIntervalEvaluator.constructFactInstances(fact);
+			tmpPredicate = fact.getPredicate();
+			this.programFacts.putIfAbsent(tmpPredicate, new LinkedHashSet<>());
+			this.programFacts.get(tmpPredicate).addAll(tmpInstances);
+		}
+	}
+
+	private Map<Integer, NonGroundRule> initializeRules(Program program) {
+		Map<Integer, NonGroundRule> retVal = new HashMap<>();
+		NonGroundRule tmpRule;
+		for (Rule rule : program.getRules()) {
+			tmpRule = NonGroundRule.constructNonGroundRule(rule);
+			this.nonGroundRules.put(tmpRule.getRuleId(), tmpRule);
+			if (tmpRule.getHeadAtom() != null) {
+				// rule is not a constraint, register the predicate it defines
+				this.recordDefiningRule(tmpRule.getHeadAtom().getPredicate(), tmpRule);
+			}
+		}
+		return retVal;
 	}
 
 	public void recordDefiningRule(Predicate headPredicate, NonGroundRule rule) {
-		predicateDefiningRules.putIfAbsent(headPredicate, new HashSet<>());
-		predicateDefiningRules.get(headPredicate).add(rule);
+		this.predicateDefiningRules.putIfAbsent(headPredicate, new HashSet<>());
+		this.predicateDefiningRules.get(headPredicate).add(rule);
 	}
 
 	public Map<Predicate, HashSet<NonGroundRule>> getPredicateDefiningRules() {
-		return Collections.unmodifiableMap(predicateDefiningRules);
+		return Collections.unmodifiableMap(this.predicateDefiningRules);
+	}
+
+	public Map<Predicate, LinkedHashSet<Instance>> getProgramFacts() {
+		return this.programFacts;
+	}
+
+	public Map<Integer, NonGroundRule> getNonGroundRules() {
+		return this.nonGroundRules;
 	}
 }
