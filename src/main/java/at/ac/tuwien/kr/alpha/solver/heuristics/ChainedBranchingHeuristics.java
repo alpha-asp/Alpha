82df2642ca0858159224b144f1a1996104f9e8cb
Richard Taupe <richard.taupe@siemens.com>
2019-03-27 13:28:36 +0100

    Extend decision counters when heuristic is added to chain

--- src/main/java/at/ac/tuwien/kr/alpha/solver/heuristics/ChainedBranchingHeuristics.java
+++ src/main/java/at/ac/tuwien/kr/alpha/solver/heuristics/ChainedBranchingHeuristics.java
@@ -1,17 +1,17 @@
 /**
  * Copyright (c) 2018-2019 Siemens AG
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1) Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
- * 
+ *
  * 2) Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -139,6 +139,9 @@ public class ChainedBranchingHeuristics implements BranchingHeuristic {
 			throw oops("Cycle detected in chain of branching heuristics");
 		}
 		chain.add(element);
+		if (decisionCounters != null) {
+			decisionCounters = Arrays.copyOf(decisionCounters, decisionCounters.length + 1);
+		}
 	}
 
 	public BranchingHeuristic getLastElement() {
