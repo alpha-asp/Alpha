f1ef1a32683bb7ef7a54f7f4df259a8e95e60a35
Michael Langowski <e1426581@student.tuwien.ac.at>
2019-05-06 13:23:31 +0200

    intermediate commit, skeleton for evaluation

--- src/main/java/at/ac/tuwien/kr/alpha/grounder/transformation/impl/PartialEvaluation.java
+++ src/main/java/at/ac/tuwien/kr/alpha/grounder/transformation/impl/PartialEvaluation.java
@@ -15,6 +15,7 @@ import at.ac.tuwien.kr.alpha.common.program.impl.InternalProgram;
 import at.ac.tuwien.kr.alpha.common.rule.impl.InternalRule;
 import at.ac.tuwien.kr.alpha.grounder.Instance;
 import at.ac.tuwien.kr.alpha.grounder.RuleGroundingOrder;
+import at.ac.tuwien.kr.alpha.grounder.WorkingMemory;
 import at.ac.tuwien.kr.alpha.grounder.transformation.ProgramTransformation;
 
 /**
@@ -23,8 +24,11 @@ import at.ac.tuwien.kr.alpha.grounder.transformation.ProgramTransformation;
  * Copyright (c) 2019, the Alpha Team.
  */
 // TODO maybe use NormalProgram as input type, could do dependency graph right here
+// TODO ideally return "PartiallyEvaluatedProgram" here, grounder can use working memories created here rather than re-initialize everything
 public class PartialEvaluation extends ProgramTransformation<InternalProgram, InternalProgram> {
 
+	private WorkingMemory workingMemory = new WorkingMemory();
+	
 	@Override
 	public InternalProgram apply(InternalProgram inputProgram) {
 		// first, we calculate a component graph and stratification
@@ -33,20 +37,46 @@ public class PartialEvaluation extends ProgramTransformation<InternalProgram, In
 		Map<Predicate, HashSet<InternalRule>> predicateDefiningRules = inputProgram.getPredicateDefiningRules();
 		// set up list of atoms which are known to be true - we expand on this one
 		Map<Predicate, Set<Instance>> knownFacts = new LinkedHashMap<>(inputProgram.getFactsByPredicate());
+		for(Map.Entry<Predicate, Set<Instance>> entry : knownFacts.entrySet()) {
+			this.workingMemory.initialize(entry.getKey());
+			this.workingMemory.addInstances(entry.getKey(), true, entry.getValue());
+		}
 		List<SCComponent> componentHandlingList = stratification.getComponentHandlingOrder();
 		for (SCComponent currComp : componentHandlingList) {
-			this.evaluateComponent(currComp, knownFacts, predicateDefiningRules);
+			this.evaluateComponent(currComp, predicateDefiningRules);
 		}
 		return null;
 	}
 
-	private void evaluateComponent(SCComponent comp, Map<Predicate, Set<Instance>> factStore, Map<Predicate, HashSet<InternalRule>> predicateDefiningRules) {
+	private void evaluateComponent(SCComponent comp, Map<Predicate, HashSet<InternalRule>> predicateDefiningRules) {
 		Set<InternalRule> rulesToEvluate = comp.getNodes().stream().map(Node::getPredicate).map((p) -> predicateDefiningRules.getOrDefault(p, new HashSet<>()))
 				.reduce(PartialEvaluation::mergeIfNotNull).orElse(new HashSet<>());
+		boolean factsAdded;
+		// delta = cumulated facts;
+		// do while delta changes
+		// 		for each atom : delta:
+		// 			check if is starting literal from rule grounding order
+		// 			if(rule.canFire) { delta.add(ruleHead); cumulFacts.add(ruleHead) }
+		//			// for canFire copy/modify bindNextAtomInRule from grounder
+		// 		re-init delta from found substitutions
+		
+		// potential changes f. bindNextAt...:
+		// 		- check positive atoms really in facts (not in master, but not just call it, grounder my be more optimistic)
+		//		- check negative body for absence! (doesn't occur in positive facts, works because program is stratified)	
+		do {
+			
+			factsAdded = false;
+			for (InternalRule r : rulesToEvluate) {
+				factsAdded |= this.evaluateRule(r);
+			}
+		} while (factsAdded); // if size stays the same we got a fixed point
+		
 	}
 
-	private void evaluateRule(Map<Predicate, Set<Instance>> factStore, InternalRule rule) {
+	private boolean evaluateRule(InternalRule rule) {
 		RuleGroundingOrder groundingOrder = rule.getGroundingOrder();
+		// doSomething
+		return this.workingMemory.modified().isEmpty(); // TODO return if evaluation added something
 	}
 
 	private static <T> HashSet<T> mergeIfNotNull(HashSet<T> a, HashSet<T> b) {
