ad7c8de8e8c65e9a5260393a6004b09405e67235
Richard Taupe <richard.taupe@siemens.com>
2019-05-14 15:45:14 +0200

    Refactor NoGoodGenerator

--- src/test/java/at/ac/tuwien/kr/alpha/grounder/NaiveGrounderTest.java
+++ src/test/java/at/ac/tuwien/kr/alpha/grounder/NaiveGrounderTest.java
@@ -139,7 +139,7 @@ public class NaiveGrounderTest {
 		NonGroundRule nonGroundRule = NonGroundRule.constructNonGroundRule(rule);
 		Substitution substitution = new Substitution();
 		substitution.unifyTerms(N, ONE);
-		List<NoGood> generatedNoGoods = noGoodGenerator.generateNoGoodsFromGroundSubstitution(nonGroundRule, substitution);
+		List<NoGood> generatedNoGoods = new ArrayList<>(noGoodGenerator.generateNoGoodsFromGroundSubstitution(nonGroundRule, substitution));
 		assertEquals(2, generatedNoGoods.size());
 		assertEquals("*{-(HeuOff(\"0\")), +(b(1))}", atomStore.noGoodToString(generatedNoGoods.get(0)));
 		assertEquals("*{-(HeuOn(\"0\")), +(a(1))}", atomStore.noGoodToString(generatedNoGoods.get(1)));
