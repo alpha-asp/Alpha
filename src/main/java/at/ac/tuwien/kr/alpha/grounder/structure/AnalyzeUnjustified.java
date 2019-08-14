3b6d67459509434aa3ee4577770c1d50cf41e9e4
Richard Taupe <richard.taupe@siemens.com>
2019-08-14 10:26:30 +0200

    Mark inline TODO as done

--- src/main/java/at/ac/tuwien/kr/alpha/grounder/structure/AnalyzeUnjustified.java
+++ src/main/java/at/ac/tuwien/kr/alpha/grounder/structure/AnalyzeUnjustified.java
@@ -1,3 +1,30 @@
+/**
+ * Copyright (c) 2018-2019, the Alpha Team.
+ * All rights reserved.
+ *
+ * Additional changes made by Siemens.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1) Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2) Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package at.ac.tuwien.kr.alpha.grounder.structure;
 
 import at.ac.tuwien.kr.alpha.common.*;
@@ -14,9 +41,6 @@ import java.util.*;
 
 import static at.ac.tuwien.kr.alpha.Util.oops;
 
-/**
- * Copyright (c) 2018, the Alpha Team.
- */
 public class AnalyzeUnjustified {
 	private static final Logger LOGGER = LoggerFactory.getLogger(AnalyzeUnjustified.class);
 	private final ProgramAnalysis programAnalysis;
@@ -278,7 +302,7 @@ public class AnalyzeUnjustified {
 		private final Iterator<Atom> assignedAtomsIterator;
 		private final Iterator<Instance> factsIterator;
 
-		public AssignedAtomsIterator(Predicate predicate, List<Atom> assignedAtoms, Set<Instance> facts) {
+		AssignedAtomsIterator(Predicate predicate, List<Atom> assignedAtoms, Set<Instance> facts) {
 			this.predicate = predicate;
 			this.assignedAtomsIterator = assignedAtoms == null ? Collections.emptyIterator() : assignedAtoms.iterator();
 			this.factsIterator = facts == null ? Collections.emptyIterator() : facts.iterator();
@@ -308,7 +332,7 @@ public class AnalyzeUnjustified {
 		Predicate predicate = p.getPredicate();
 		// Check if literal is built-in with a fixed interpretation.
 		if (p.toLiteral() instanceof FixedInterpretationLiteral) {
-			// TODO: can we determine if p is built-in with a fixed interpretation without converting it to a literal?
+			// p.toLiteral() is necessary because the FixedInterpretationLiteral interface cannot be implemented (in general) by corresponding atoms
 			return Collections.emptyList();
 		}
 		ArrayList<FactOrNonGroundRule> definingRulesAndFacts = new ArrayList<>();
@@ -340,6 +364,7 @@ public class AnalyzeUnjustified {
 				headAtom = ((DisjunctiveHead) rule.getHead()).disjunctiveAtoms.get(0);
 			} else {
 				// Create atom and empty rule body out of instance.
+				assert factOrNonGroundRule.factInstance != null;
 				headAtom = new BasicAtom(p.getPredicate(), factOrNonGroundRule.factInstance.terms);
 				renamedBody = Collections.emptyList();
 			}
