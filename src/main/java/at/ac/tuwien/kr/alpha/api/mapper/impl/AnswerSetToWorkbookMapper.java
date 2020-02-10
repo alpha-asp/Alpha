e3b4b2dae897babc08a0e7793ad7b2278f2e71d2
Michael Langowski <e1426581@student.tuwien.ac.at>
2020-02-10 15:38:55 +0100

    implementation and tests for mapping from AnswerSet to Workbook

--- src/main/java/at/ac/tuwien/kr/alpha/api/mapper/impl/AnswerSetToWorkbookMapper.java
+++ src/main/java/at/ac/tuwien/kr/alpha/api/mapper/impl/AnswerSetToWorkbookMapper.java
@@ -25,12 +25,22 @@
  */
 package at.ac.tuwien.kr.alpha.api.mapper.impl;
 
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import at.ac.tuwien.kr.alpha.api.mapper.AnswerSetToObjectMapper;
 import at.ac.tuwien.kr.alpha.common.AnswerSet;
+import at.ac.tuwien.kr.alpha.common.Predicate;
+import at.ac.tuwien.kr.alpha.common.atoms.Atom;
+import at.ac.tuwien.kr.alpha.common.atoms.BasicAtom;
+import at.ac.tuwien.kr.alpha.common.terms.Term;
 
 /**
  * Implementation of {@link AnswerSetToObjectMapper} that generates an office open xml workbook ("excel file") from a given answer set.
@@ -41,10 +51,55 @@ public class AnswerSetToWorkbookMapper implements AnswerSetToObjectMapper<Workbo
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(AnswerSetToWorkbookMapper.class);
 
+	/**
+	 * Creates an xlsx workbook containing all the atoms from the given {@link AnswerSet} with one sheet per predicate. All predicates with arity 0 are listed
+	 * in a special sheet called "flags". Caution, potential resource leak: note that the returned workbook needs to be closed by the caller once it has been
+	 * processed (written to file etc).
+	 */
 	@Override
 	public Workbook mapFromAnswerSet(AnswerSet answerSet) {
-		// TODO Auto-generated method stub
-		return null;
+		LOGGER.debug("Start mapping answer set to workbook");
+		Workbook workbook = new XSSFWorkbook();
+		// first, create a worksheet for 0-arity predicates
+		Sheet flags = workbook.createSheet("Flags");
+		Sheet currentPredicateSheet;
+		for (Predicate pred : answerSet.getPredicates()) {
+			if (pred.getArity() == 0) {
+				// 0-artiy predicate has no instances in answer set, create a dummy atom
+				this.writeAtomToSheet(flags, new BasicAtom(pred));
+			} else {
+				currentPredicateSheet = workbook.createSheet(pred.getName() + "_" + pred.getArity());
+				for (Atom atom : answerSet.getPredicateInstances(pred)) {
+					this.writeAtomToSheet(currentPredicateSheet, atom);
+				}
+			}
+		}
+		return workbook;
+	}
+
+	private void writeAtomToSheet(Sheet sheet, Atom atom) {
+		int rownum = -1;
+		if (sheet.getLastRowNum() == 0 && sheet.getRow(0) == null) {
+			// sheet is empty, start at row zero
+			rownum = 0;
+		} else {
+			rownum = sheet.getLastRowNum() + 1;
+		}
+		Row atomRow = sheet.createRow(rownum);
+		List<Term> terms = atom.getTerms();
+		Cell currCell;
+		if (terms.isEmpty()) {
+			// 0-arity atom
+			currCell = atomRow.createCell(0);
+			currCell.setCellValue(atom.getPredicate().getName());
+			sheet.autoSizeColumn(0);
+		} else {
+			for (int i = 0; i < terms.size(); i++) {
+				currCell = atomRow.createCell(i);
+				currCell.setCellValue(terms.get(i).toString());
+				sheet.autoSizeColumn(i);
+			}
+		}
 	}
 
 }
---
+++ src/test/java/at/ac/tuwien/kr/alpha/api/mapper/impl/AnswerSetToWorkbookMapperTest.java
@@ -0,0 +1,98 @@
+package at.ac.tuwien.kr.alpha.api.mapper.impl;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.junit.Assert;
+import org.junit.Test;
+
+import at.ac.tuwien.kr.alpha.api.Alpha;
+import at.ac.tuwien.kr.alpha.common.AnswerSet;
+import at.ac.tuwien.kr.alpha.common.AnswerSetBuilder;
+import at.ac.tuwien.kr.alpha.common.Predicate;
+import at.ac.tuwien.kr.alpha.common.atoms.Atom;
+import at.ac.tuwien.kr.alpha.common.terms.Term;
+
+public class AnswerSetToWorkbookMapperTest {
+
+	private AnswerSetToWorkbookMapper mapper = new AnswerSetToWorkbookMapper();
+
+	@Test
+	public void smokeTest() throws IOException {
+		AnswerSet as = new AnswerSetBuilder().predicate("bla").instance("blubb", "blubb").instance("foo", "bar").predicate("foo").instance("bar")
+				.instance("baz").predicate("complex").instance(Integer.valueOf(1), Integer.valueOf(2), Integer.valueOf(3)).build();
+		Workbook wb = this.mapper.mapFromAnswerSet(as);
+		Assert.assertNotNull(wb.getSheet("Flags"));
+		Assert.assertNotNull(wb.getSheet("bla_2"));
+		Assert.assertNotNull(wb.getSheet("foo_1"));
+		Assert.assertNotNull(wb.getSheet("complex_3"));
+		wb.close();
+	}
+
+	@Test
+	public void solveAndWriteWorkbookTest() {
+		//@formatter:off
+		String progstr = "aFlag. oneMoreFlag. yetAnotherFlag. createPs. maxP(5). r(s(1, 2, 3), 4). r(bla, blubb). r(foo, bar(baaz))."
+				+ "p(0) :- createPs. "
+				+ "p(N) :- p(I), N = I + 1, N <= MX, maxP(MX)."
+				+ "q(A, B) :- p(A), p(B).";
+		//@formatter:on
+		Alpha alpha = new Alpha();
+		List<AnswerSet> answerSets = alpha.solve(alpha.readProgramString(progstr, null)).collect(Collectors.toList());
+		Assert.assertEquals(1, answerSets.size());
+		AnswerSet as = answerSets.get(0);
+		Workbook answerSetWorkbook = this.mapper.mapFromAnswerSet(as);
+		AnswerSetToWorkbookMapperTest.assertWorkbookMatchesAnswerSet(answerSetWorkbook, as);
+	}
+
+	private static void assertWorkbookMatchesAnswerSet(Workbook wb, AnswerSet as) {
+		for (Predicate pred : as.getPredicates()) {
+			if (pred.getArity() == 0) {
+				boolean flagFound = false;
+				Sheet flagsSheet = wb.getSheet("Flags");
+				Assert.assertNotNull(flagsSheet);
+				for (Row row : flagsSheet) {
+					if (row.getCell(0).getStringCellValue().equals(pred.getName())) {
+						flagFound = true;
+						break;
+					}
+				}
+				Assert.assertTrue("0-arity predicate " + pred.getName() + " not found in workbook!", flagFound);
+			} else {
+				Sheet predicateSheet = wb.getSheet(pred.getName() + "_" + pred.getArity());
+				for (Atom atom : as.getPredicateInstances(pred)) {
+					boolean atomFound = false;
+					Assert.assertNotNull(predicateSheet);
+					for (Row row : predicateSheet) {
+						if (AnswerSetToWorkbookMapperTest.rowMatchesAtom(row, atom)) {
+							atomFound = true;
+							break;
+						}
+					}
+					Assert.assertTrue("Atom " + atom.toString() + " not found in workbook!", atomFound);
+				}
+			}
+		}
+	}
+
+	private static boolean rowMatchesAtom(Row row, Atom atom) {
+		List<Term> terms = atom.getTerms();
+		Cell cell;
+		for (int i = 0; i < terms.size(); i++) {
+			cell = row.getCell(i);
+			if (cell == null) {
+				return false;
+			}
+			if (!(cell.getStringCellValue().equals(terms.get(i).toString()))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
