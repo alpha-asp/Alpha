8f1d5a2058b2638a0fd111531ba369d6fa87c1be
Michael Langowski <e1426581@student.tuwien.ac.at>
2020-02-17 14:10:07 +0100

    additional test case for writing unsat info

--- src/test/java/at/ac/tuwien/kr/alpha/AnswerSetToXlsxWriterTest.java
+++ src/test/java/at/ac/tuwien/kr/alpha/AnswerSetToXlsxWriterTest.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.junit.Assert;
@@ -30,8 +33,31 @@ public class AnswerSetToXlsxWriterTest {
 		Assert.assertEquals("alphaAnswerSet.0.xlsx", answerSetFile.getName());
 		Workbook wb = WorkbookFactory.create(answerSetFile);
 		AnswerSetToWorkbookMapperTest.assertWorkbookMatchesAnswerSet(wb, as);
+		wb.close();
 		// clean up
 		answerSetFile.delete();
+		tmpDirFile.delete();
+	}
+
+	@Test
+	public void writeUnsatTest() throws IOException {
+		Path tmpDir = Files.createTempDirectory("alpha-test-xlsx-unsat");
+		AnswerSetToXlsxWriter.writeUnsatInfo(Paths.get(tmpDir.toString() + "/alphaAnswerSet.UNSAT.xlsx"));
+		File tmpDirFile = tmpDir.toFile();
+		File[] generatedFiles = tmpDirFile.listFiles();
+		Assert.assertEquals(generatedFiles.length, 1);
+		File unsatFile = generatedFiles[0];
+		Assert.assertEquals("alphaAnswerSet.UNSAT.xlsx", unsatFile.getName());
+		Workbook wb = WorkbookFactory.create(unsatFile);
+		Sheet unsatSheet = wb.getSheet("Unsatisfiable");
+		Assert.assertNotNull(unsatSheet);
+		Cell cell = unsatSheet.getRow(0).getCell(0);
+		Assert.assertNotNull(cell);
+		String cellValue = cell.getStringCellValue();
+		Assert.assertEquals("Input is unsatisfiable - No answer sets!", cellValue);
+		wb.close();
+		// clean up
+		unsatFile.delete();
 		tmpDirFile.delete();
 	}
 
