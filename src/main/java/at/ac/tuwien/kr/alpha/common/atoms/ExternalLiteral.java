d3f973a32814f2dd2a53d9cb43f4766f318ba2ac
Michael Langowski <e1426581@student.tuwien.ac.at>
2020-04-03 18:59:24 +0200

    partial fix, still one test failing

--- src/main/java/at/ac/tuwien/kr/alpha/common/atoms/ExternalLiteral.java
+++ src/main/java/at/ac/tuwien/kr/alpha/common/atoms/ExternalLiteral.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017-2018, the Alpha Team.
+ * Copyright (c) 2017-2020, the Alpha Team.
  * All rights reserved.
  * 
  * Additional changes made by Siemens.
@@ -27,14 +27,16 @@
  */
 package at.ac.tuwien.kr.alpha.common.atoms;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import at.ac.tuwien.kr.alpha.common.terms.ConstantTerm;
 import at.ac.tuwien.kr.alpha.common.terms.Term;
 import at.ac.tuwien.kr.alpha.common.terms.VariableTerm;
 import at.ac.tuwien.kr.alpha.grounder.Substitution;
-
-import java.util.*;
-
-import static java.util.Collections.emptyList;
 
 /**
  * Contains a potentially negated {@link ExternalAtom}.
@@ -44,14 +46,15 @@ public class ExternalLiteral extends FixedInterpretationLiteral {
 	public ExternalLiteral(ExternalAtom atom, boolean positive) {
 		super(atom, positive);
 	}
-	
+
 	@Override
 	public ExternalAtom getAtom() {
-		return (ExternalAtom)atom;
+		return (ExternalAtom) atom;
 	}
 
 	/**
-	 * Returns a new copy of this literal whose {@link Literal#isNegated()} status is inverted
+	 * Returns a new copy of this literal whose {@link Literal#isNegated()} status
+	 * is inverted
 	 */
 	@Override
 	public ExternalLiteral negate() {
@@ -68,14 +71,15 @@ public class ExternalLiteral extends FixedInterpretationLiteral {
 
 	@Override
 	public Set<VariableTerm> getBindingVariables() {
-		// If the external atom is negative, then all variables of input and output are non-binding
+		// If the external atom is negative, then all variables of input and output are
+		// non-binding
 		// and there are no binding variables (like for ordinary atoms).
 		// If the external atom is positive, then variables of output are binding.
 
 		if (!positive) {
 			return Collections.emptySet();
 		}
-		
+
 		List<Term> output = getAtom().getOutput();
 
 		Set<VariableTerm> binding = new HashSet<>(output.size());
@@ -93,7 +97,7 @@ public class ExternalLiteral extends FixedInterpretationLiteral {
 	public Set<VariableTerm> getNonBindingVariables() {
 		List<Term> input = getAtom().getInput();
 		List<Term> output = getAtom().getOutput();
-		
+
 		// External atoms have their input always non-binding, since they cannot
 		// be queried without some concrete input.
 		Set<VariableTerm> nonbindingVariables = new HashSet<>();
@@ -101,7 +105,8 @@ public class ExternalLiteral extends FixedInterpretationLiteral {
 			nonbindingVariables.addAll(term.getOccurringVariables());
 		}
 
-		// If the external atom is negative, then all variables of input and output are non-binding.
+		// If the external atom is negative, then all variables of input and output are
+		// non-binding.
 		if (!positive) {
 			for (Term out : output) {
 				if (out instanceof VariableTerm) {
@@ -116,33 +121,53 @@ public class ExternalLiteral extends FixedInterpretationLiteral {
 	@Override
 	public List<Substitution> getSubstitutions(Substitution partialSubstitution) {
 		List<Term> input = getAtom().getInput();
-		List<Term> output = getAtom().getOutput();
-		List<Substitution> substitutions = new ArrayList<>();
 		List<Term> substitutes = new ArrayList<>(input.size());
 
+		// evaluate the external atom
 		for (Term t : input) {
 			substitutes.add(t.substitute(partialSubstitution));
 		}
-
 		Set<List<ConstantTerm<?>>> results = getAtom().getInterpretation().evaluate(substitutes);
-
 		if (results == null) {
 			throw new NullPointerException("Predicate " + getPredicate().getName() + " returned null. It must return a Set.");
 		}
 
+		// determine ground substitutions of the literal
 		if (results.isEmpty()) {
-			return emptyList();
-		}
-
-		for (List<ConstantTerm<?>> bindings : results) {
-			if (bindings.size() < output.size()) {
-				throw new RuntimeException("Predicate " + getPredicate().getName() + " returned " + bindings.size() + " terms when at least " + output.size() + " were expected.");
+			// if the literal is negated and results are empty, the literal is "true",
+			// i.e. "not ATOM" holds because there are no instances of ATOM.
+			// communicate this by returning the partial substitution: since a negated
+			// external doesn't bind anything, it doesn't contribute to the full
+			// substitution of the rule body
+			if (this.isNegated()) {
+				List<Substitution> retVal = new ArrayList<>();
+				retVal.add(partialSubstitution);
+				return retVal;
+			} else {
+				return Collections.emptyList();
 			}
+		} else {
+			if (this.isNegated()) {
+				return Collections.emptyList();
+			} else {
+				return this.buildSubstitutionsForOutputs(partialSubstitution, results);
+			}
+		}
+	}
 
+	private List<Substitution> buildSubstitutionsForOutputs(Substitution partialSubstitution, Set<List<ConstantTerm<?>>> outputs) {
+		List<Substitution> retVal = new ArrayList<>();
+		List<Term> externalAtomOutputTerms = this.getAtom().getOutput();
+		for (List<ConstantTerm<?>> bindings : outputs) {
+			if (bindings.size() < externalAtomOutputTerms.size()) {
+				throw new RuntimeException(
+						"Predicate " + getPredicate().getName() + " returned " + bindings.size() + " terms when at least " + externalAtomOutputTerms.size()
+								+ " were expected.");
+			}
 			Substitution ith = new Substitution(partialSubstitution);
 			boolean skip = false;
-			for (int i = 0; i < output.size(); i++) {
-				Term out = output.get(i);
+			for (int i = 0; i < externalAtomOutputTerms.size(); i++) {
+				Term out = externalAtomOutputTerms.get(i);
 
 				if (out instanceof VariableTerm) {
 					ith.put((VariableTerm) out, bindings.get(i));
@@ -153,13 +178,11 @@ public class ExternalLiteral extends FixedInterpretationLiteral {
 					}
 				}
 			}
-
 			if (!skip) {
-				substitutions.add(ith);
+				retVal.add(ith);
 			}
 		}
-
-		return substitutions;
+		return retVal;
 	}
 
 }
