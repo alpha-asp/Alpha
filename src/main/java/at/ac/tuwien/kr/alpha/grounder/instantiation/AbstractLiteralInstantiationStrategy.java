f503c04bb15b634d5401bdd2a747184232a46968
Michael Langowski <e1426581@student.tuwien.ac.at>
2020-07-01 20:01:03 +0200

    template pattern for LiteralInstantiationStrategy

---
+++ src/main/java/at/ac/tuwien/kr/alpha/grounder/instantiation/AbstractLiteralInstantiationStrategy.java
@@ -0,0 +1,132 @@
+package at.ac.tuwien.kr.alpha.grounder.instantiation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
+import at.ac.tuwien.kr.alpha.common.atoms.Atom;
+import at.ac.tuwien.kr.alpha.common.atoms.BasicAtom;
+import at.ac.tuwien.kr.alpha.common.atoms.Literal;
+import at.ac.tuwien.kr.alpha.grounder.Instance;
+import at.ac.tuwien.kr.alpha.grounder.Substitution;
+
+/**
+ * Abstract base implementation of {@link LiteralInstantiationStrategy} that
+ * outlines a basic workflow for
+ * {@link LiteralInstantiationStrategy#getTruthForGroundLiteral(Literal)} and
+ * {@link LiteralInstantiationStrategy#getAcceptedSubstitutions(Literal, Substitution)}
+ * while leaving details of when an atom is true and which
+ * {@link AssignmentStatus}es to consider valid for
+ * <code>getAcceptedSubstitutions</code> to implementations.
+ * 
+ * Copyright (c) 2020, the Alpha Team.
+ */
+public abstract class AbstractLiteralInstantiationStrategy implements LiteralInstantiationStrategy {
+
+	/**
+	 * See {@link LiteralInstantiationStrategy#getTruthForGroundLiteral(Literal).
+	 * 
+	 * In general - since this code is used in a grounding context where negative
+	 * literals in rule bodies can be handled in different ways - the logic for
+	 * determining the {@link AssignmentStatus} of a negated literal is delegated to
+	 * the abstract method
+	 * {@link AbstractLiteralInstantiationStrategy#getAssignmentStatusForNegatedGroundLiteral(Literal)}.
+	 * The assignment status for positive literals is determined using the abstract
+	 * method
+	 * {@link AbstractLiteralInstantiationStrategy#getAssignmentStatusForAtom(Atom)}
+	 */
+	@Override
+	public final AssignmentStatus getTruthForGroundLiteral(Literal groundLiteral) {
+		if (groundLiteral.isNegated()) {
+			return this.getAssignmentStatusForNegatedGroundLiteral(groundLiteral);
+		}
+		return this.getAssignmentStatusForAtom(groundLiteral.getAtom());
+	}
+
+	/**
+	 * See
+	 * {@link LiteralInstantiationStrategy#getAcceptedSubstitutions(Literal, Substitution)}.
+	 * 
+	 * A very general implementation of the basic steps needed to obtain ground
+	 * substitutions for a positive literal.
+	 * Potentially valid ground instances are obtained using
+	 * {@link AbstractLiteralInstantiationStrategy#computeCandidateInstances(Atom)},
+	 * then checked, such that each candidate instance has unifies with the given
+	 * partial substitution and has a "valid" {@link AssignmentStatus},
+	 * where "validity" of an {@link AssignmentStatus} is determined using the
+	 * abstract method
+	 * {@link AbstractLiteralInstantiationStrategy#assignmentStatusAccepted(AssignmentStatus)}
+	 */
+	@Override
+	public final List<ImmutablePair<Substitution, AssignmentStatus>> getAcceptedSubstitutions(Literal lit, Substitution partialSubstitution) {
+		Atom atom = lit.getAtom();
+		Iterable<Instance> groundInstances = this.computeCandidateInstances(atom);
+		return this.buildSubstitutionsFromInstances(atom, groundInstances, partialSubstitution);
+	}
+
+	/**
+	 * Computes instances that are potentially valid ground instances of the given
+	 * partially-ground atom.
+	 * 
+	 * A candidate instance is a ground instance of the same predicate where all
+	 * terms that are ground in <code>partiallyGroundAtom</code> have the same
+	 * values in the candidate.
+	 * 
+	 * @param partiallyGroundAtom a partially ground atom for which to find fitting
+	 *                            ground instances
+	 * @return a list of candidate instances
+	 */
+	protected abstract Iterable<Instance> computeCandidateInstances(Atom partiallyGroundAtom);
+
+	/**
+	 * Based on a list of candidate instances (see
+	 * {@link AbstractLiteralInstantiationStrategy#computeCandidateInstances(Atom)}),
+	 * create a list of substitutions and assignment statuses such that each
+	 * substitution represents a valid (according to the implementation-specific
+	 * definition of this instantiation strategy) ground instance of
+	 * <code>atomToSubstitute</code>
+	 * 
+	 * @param atomToSubstitute
+	 * @param candidateInstances
+	 * @param partialSubstitution
+	 * @return
+	 */
+	protected final List<ImmutablePair<Substitution, AssignmentStatus>> buildSubstitutionsFromInstances(Atom atomToSubstitute,
+			Iterable<Instance> candidateInstances, Substitution partialSubstitution) {
+		List<ImmutablePair<Substitution, AssignmentStatus>> retVal = new ArrayList<>();
+		// Filter for only instances unifying with partialSubsitution,
+		// i.e. "where all joins work out".
+		Substitution currentInstanceSubstitution;
+		AssignmentStatus truthForCurrentAtom;
+		Atom atomForCurrentInstance;
+		for (Instance instance : candidateInstances) {
+			currentInstanceSubstitution = Substitution.unify(atomToSubstitute, instance, new Substitution(partialSubstitution));
+			if (currentInstanceSubstitution == null) {
+				// Instance does not unify with partialSubstitution,
+				// move on to the next instance
+				continue;
+			}
+			// At this point, we know that the substitution works out.
+			// Now check that the resulting Atom is either true or unassigned
+			atomForCurrentInstance = new BasicAtom(atomToSubstitute.getPredicate(), atomToSubstitute.getTerms())
+					.substitute(currentInstanceSubstitution);
+			AssignmentStatus assignmentStatus = this.getAssignmentStatusForAtom(atomForCurrentInstance);
+			truthForCurrentAtom = this.getAssignmentStatusForAtom(atomForCurrentInstance);
+			if (!this.assignmentStatusAccepted(assignmentStatus)) {
+				// Atom has an assignment status deemed unacceptable by this instantiation
+				// strategy
+				continue;
+			}
+			retVal.add(new ImmutablePair<>(currentInstanceSubstitution, truthForCurrentAtom));
+		}
+		return retVal;
+	}
+
+	protected abstract AssignmentStatus getAssignmentStatusForAtom(Atom atom);
+
+	protected abstract AssignmentStatus getAssignmentStatusForNegatedGroundLiteral(Literal negatedGroundLiteral);
+
+	protected abstract boolean assignmentStatusAccepted(AssignmentStatus assignmentStatus);
+
+}
--- src/main/java/at/ac/tuwien/kr/alpha/grounder/instantiation/DefaultLazyGroundingInstantiationStrategy.java
+++ src/main/java/at/ac/tuwien/kr/alpha/grounder/instantiation/DefaultLazyGroundingInstantiationStrategy.java
@@ -1,28 +1,19 @@
 package at.ac.tuwien.kr.alpha.grounder.instantiation;
 
-import java.util.ArrayList;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import at.ac.tuwien.kr.alpha.Util;
 import at.ac.tuwien.kr.alpha.common.Assignment;
 import at.ac.tuwien.kr.alpha.common.AtomStore;
 import at.ac.tuwien.kr.alpha.common.Predicate;
 import at.ac.tuwien.kr.alpha.common.atoms.Atom;
-import at.ac.tuwien.kr.alpha.common.atoms.BasicAtom;
 import at.ac.tuwien.kr.alpha.common.atoms.Literal;
 import at.ac.tuwien.kr.alpha.grounder.IndexedInstanceStorage;
 import at.ac.tuwien.kr.alpha.grounder.Instance;
-import at.ac.tuwien.kr.alpha.grounder.Substitution;
 import at.ac.tuwien.kr.alpha.grounder.WorkingMemory;
 
-public class DefaultLazyGroundingInstantiationStrategy implements LiteralInstantiationStrategy {
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(DefaultLazyGroundingInstantiationStrategy.class);
+public class DefaultLazyGroundingInstantiationStrategy extends AbstractLiteralInstantiationStrategy {
 
 	private WorkingMemory workingMemory;
 	private AtomStore atomStore;
@@ -37,6 +28,40 @@ public class DefaultLazyGroundingInstantiationStrategy implements LiteralInstant
 		this.facts = facts;
 	}
 
+	@Override
+	protected Iterable<Instance> computeCandidateInstances(Atom partiallyGroundAtom) {
+		IndexedInstanceStorage instanceStorage = this.workingMemory.get(partiallyGroundAtom, true);
+		return instanceStorage.getInstancesFromPartiallyGroundAtom(partiallyGroundAtom);
+	}
+
+	@Override
+	protected AssignmentStatus getAssignmentStatusForAtom(Atom atom) {
+		if (this.currentAssignment == null || this.isFact(atom)) {
+			// currentAssignment == null is a legitimate case, grounder may be in bootstrap
+			// and will call bindNextAtom with
+			// null assignment in that case
+			// Assumption: since the atom came from working memory and we must be in
+			// bootstrap here, we can assume for the atom to be true (or atom is a fact
+			// anyway, in which case it's also true)
+			return AssignmentStatus.TRUE;
+		}
+		AssignmentStatus retVal;
+		// First, make sure that the Atom in question exists in the AtomStore
+		int atomId = this.atomStore.putIfAbsent(atom);
+		// newly obtained atomId might be higher than the maximum in the current
+		// assignment, grow the assignment
+		this.currentAssignment.growForMaxAtomId();
+		if (currentAssignment.isAssigned(atomId)) {
+			retVal = currentAssignment.getTruth(atomId).toBoolean() ? AssignmentStatus.TRUE : AssignmentStatus.FALSE;
+		} else {
+			retVal = AssignmentStatus.UNASSIGNED;
+		}
+		if (retVal == AssignmentStatus.FALSE || retVal == AssignmentStatus.UNASSIGNED) {
+			this.staleWorkingMemoryEntries.add(atom);
+		}
+		return retVal;
+	}
+
 	private boolean isFact(Atom atom) {
 		if (this.facts.get(atom.getPredicate()) == null) {
 			return false;
@@ -46,97 +71,21 @@ public class DefaultLazyGroundingInstantiationStrategy implements LiteralInstant
 	}
 
 	@Override
-	public AssignmentStatus getTruthForGroundLiteral(Literal groundLiteral) {
-		AssignmentStatus retVal;
-		if (groundLiteral.isNegated()) {
-			// In the context of this InstantiationStrategy, we consider negated literals
-			// to be always true
-			retVal = AssignmentStatus.TRUE;
-		} else {
-			Atom atom = groundLiteral.getAtom();
-			if (this.isFact(atom)) {
-				retVal = AssignmentStatus.TRUE;
-			} else {
-				retVal = this.getTruthValueForAtom(atom);
-			}
-		}
-		LOGGER.trace("Got assignmentStatus = {} for literal {}", retVal, groundLiteral);
-		return retVal;
+	protected AssignmentStatus getAssignmentStatusForNegatedGroundLiteral(Literal negatedGroundLiteral) {
+		return AssignmentStatus.TRUE;
 	}
 
-	// Contract: lit is already substituted with partialSubstitution!
-	// Result represents entries for <substitution, literalSatisfied>
-	// literalSatisfied is true iff lit is true under current assignment,
-	// false if unassigned
-	// (Note: substitutions where instances are false will be quietly discarded)
-	// TODO negative literals??
 	@Override
-	public List<ImmutablePair<Substitution, AssignmentStatus>> getAcceptedSubstitutions(Literal lit, Substitution partialSubstitution) {
-		List<ImmutablePair<Substitution, AssignmentStatus>> retVal = new ArrayList<>();
-		Atom atom = lit.getAtom();
-
-		// First, we get all ground instances from working memory that
-		// could potentially fit, according to partialSubstitution.
-		IndexedInstanceStorage instanceStorage = this.workingMemory.get(atom, true);
-		List<Instance> groundInstances = instanceStorage.getInstancesFromPartiallyGroundAtom(atom);
-
-		// Now filter for only instances unifying with partialSubsitution,
-		// i.e. "where all joins work out".
-		Substitution currentInstanceSubstitution;
-		AssignmentStatus truthForCurrentAtom;
-		Atom atomForCurrentInstance;
-		for (Instance instance : groundInstances) {
-			currentInstanceSubstitution = Substitution.unify(atom, instance, new Substitution(partialSubstitution));
-			if (currentInstanceSubstitution == null) {
-				// Instance does not unify with partialSubstitution,
-				// move on to the next instance
-				continue;
-			}
-			// At this point, we know that the substitution works out.
-			// Now check that the resulting Atom is either true or unassigned
-			atomForCurrentInstance = new BasicAtom(atom.getPredicate(), atom.getTerms())
-					.substitute(currentInstanceSubstitution);
-			truthForCurrentAtom = this.getTruthValueForAtom(atomForCurrentInstance);
-			if (truthForCurrentAtom == AssignmentStatus.FALSE) {
-				continue; // discard that instance
-			}
-			retVal.add(new ImmutablePair<>(currentInstanceSubstitution, truthForCurrentAtom));
+	protected boolean assignmentStatusAccepted(AssignmentStatus assignmentStatus) {
+		switch (assignmentStatus) {
+			case TRUE:
+			case UNASSIGNED:
+				return true;
+			case FALSE:
+				return false;
+			default:
+				throw Util.oops("Unsupported AssignmentStatus: " + assignmentStatus);
 		}
-		return retVal;
-	}
-
-	// FIXME we also need to check assignment for atoms that do not exist in working
-	// memory... 4 realz?
-	// ... is that really necessary, or rather just an artefact from test case
-	// NaiveGrounderTest#testGroundingOfRuleSwitchedOffByFalsePositiveBody???
-	private AssignmentStatus getTruthValueForAtom(Atom atom) {
-		AssignmentStatus retVal;
-		if (this.currentAssignment == null) {
-			// legitimate case, grounder may be in bootstrap and will call bindNextAtom with
-			// null assignment in that case
-			// Assumption: since the atom came from working memory and we must be in
-			// bootstrap here, we can assume for the atom to be true
-			retVal = AssignmentStatus.TRUE;
-		} else {
-			if (this.isFact(atom)) {
-				retVal = AssignmentStatus.TRUE;
-			} else {
-				// First, make sure that the Atom in question exists in the AtomStore
-				int atomId = this.atomStore.putIfAbsent(atom);
-				// newly obtained atomId might be higher than the maximum in the current
-				// assignment, grow the assignment
-				this.currentAssignment.growForMaxAtomId();
-				if (currentAssignment.isAssigned(atomId)) {
-					retVal = currentAssignment.getTruth(atomId).toBoolean() ? AssignmentStatus.TRUE : AssignmentStatus.FALSE;
-				} else {
-					retVal = AssignmentStatus.UNASSIGNED;
-				}
-			}
-		}
-		if (retVal == AssignmentStatus.FALSE || retVal == AssignmentStatus.UNASSIGNED) {
-			this.staleWorkingMemoryEntries.add(atom);
-		}
-		return retVal;
 	}
 
 	public Assignment getCurrentAssignment() {
