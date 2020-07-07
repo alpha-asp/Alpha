d10c885814f33dc7001827ea7f9bdd57453d1dcc
Michael Langowski <e1426581@student.tuwien.ac.at>
2020-07-07 13:28:47 +0200

    improve unit tests

--- src/test/java/at/ac/tuwien/kr/alpha/solver/SolverTests.java
+++ src/test/java/at/ac/tuwien/kr/alpha/solver/SolverTests.java
@@ -225,30 +225,26 @@ public class SolverTests extends AbstractSolverTests {
 	
 	@Test
 	public void choiceProgramConstraintSimple() throws IOException {
-		assertAnswerSetsWithBase(
-				"fact(x).\n" + 
+		assertAnswerSet(
+				"fact(a).\n" + 
 				"choice(either, X) :- fact(X), not choice(or, X).\n" + 
 				"choice(or, X) :- fact(X), not choice(either, X).\n" + 
 				":- choice(or, X).",
 				
-				"fact(x)",
-				
-				"choice(either, x)"
+				"fact(a), choice(either, a)"
 		);
 	}
 	
 	@Test
 	public void choiceProgramConstraintSimple2() throws IOException {
-		assertAnswerSetsWithBase(
-				"fact(x).\n" + 
+		assertAnswerSet(
+				"fact(a).\n" + 
 				"desired(either).\n" + 
 				"choice(either, X) :- fact(X), not choice(or, X).\n" + 
 				"choice(or, X) :- fact(X), not choice(either, X).\n" + 
 				":- choice(C, X), not desired(C).",
 				
-				"fact(x), desired(either)",
-				
-				"choice(either, x)"
+				"fact(a), desired(either), choice(either, a)"
 		);
 	}
 	
