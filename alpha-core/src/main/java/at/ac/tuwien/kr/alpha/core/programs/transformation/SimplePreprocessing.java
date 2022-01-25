aff3346dfb66a0d2ba1facfa4f28455be357a2aa
mschmutzhart <schmutzhartmichael@gmail.com>
2022-01-25 15:00:41 +0100

    fixed bugs in SimplePreprocessing, added tests

--- alpha-core/src/main/java/at/ac/tuwien/kr/alpha/core/programs/transformation/SimplePreprocessing.java
+++ alpha-core/src/main/java/at/ac/tuwien/kr/alpha/core/programs/transformation/SimplePreprocessing.java
@@ -22,33 +22,10 @@ public class SimplePreprocessing extends ProgramTransformation<NormalProgram, No
 	@Override
 	public NormalProgram apply(NormalProgram inputProgram) {
 		List<NormalRule> srcRules = inputProgram.getRules();
-		Set<Atom> facts = new LinkedHashSet<>(inputProgram.getFacts());
-		boolean modified = true;
-		while (modified) {
-			modified = false;
-			//implements s0 by using a Set (delete duplicate rules)
-			Set<NormalRule> newRules = new LinkedHashSet<>();
-			for (NormalRule rule : srcRules) {
-				//s9 + s10
-				NormalRule simplifiedRule = simplifyRule(rule, srcRules, facts);
-				if (simplifiedRule == null) {
-					continue;
-				}
-				else if (simplifiedRule.getBody().isEmpty()) {
-					facts.add(simplifiedRule.getHeadAtom());
-					modified = true;
-					continue;
-				} else {
-					if(!simplifiedRule.equals(rule)) {
-						modified = true;
-						rule = simplifiedRule;
-					}
-				}
-				newRules.add(rule);
-			}
-			srcRules = new LinkedList<>(newRules);
-		}
+		//Implements s0 by using a Set (delete duplicate rules).
 		Set<NormalRule> newRules = new LinkedHashSet<>();
+		Set<Atom> facts = new LinkedHashSet<>(inputProgram.getFacts());
+		boolean canBeModified = true;
 		for (NormalRule rule: srcRules) {
 			//s2
 			if (checkForConflictingBodyLiterals(rule.getPositiveBody(), rule.getNegativeBody())) {
@@ -60,7 +37,55 @@ public class SimplePreprocessing extends ProgramTransformation<NormalProgram, No
 			}
 			newRules.add(rule);
 		}
-		return new NormalProgramImpl(new LinkedList<>(newRules),new LinkedList<>(facts),inputProgram.getInlineDirectives());
+		srcRules = new LinkedList<>(newRules);
+		//s9 + s10
+		while (canBeModified) {
+			newRules = new LinkedHashSet<>();
+			canBeModified = false;
+			for (NormalRule rule : srcRules) {
+				SimpleReturn simpleReturn = simplifyRule(rule, srcRules, facts);
+				if (simpleReturn != null) {
+					if (simpleReturn == SimpleReturn.NO_FIRE) {
+						canBeModified = true;
+					} else if (simpleReturn.isFact()) {
+						facts.add(simpleReturn.getRule().getHeadAtom());
+						canBeModified = true;
+					} else if (simpleReturn.isModified()) {
+						newRules.add(simpleReturn.getRule());
+						canBeModified = true;
+					} else {
+						newRules.add(rule);
+					}
+				}
+			}
+			srcRules = new LinkedList<>(newRules);
+		}
+		return new NormalProgramImpl(new LinkedList<>(newRules), new LinkedList<>(facts), inputProgram.getInlineDirectives());
+	}
+
+	private static class SimpleReturn {
+		public final static SimpleReturn NO_FIRE = new SimpleReturn(null, false, false);
+		private final NormalRule rule;
+		private final boolean isModified;
+		private final boolean isFact;
+
+		public SimpleReturn(NormalRule rule, boolean isModified, boolean isFact) {
+			this.rule = rule;
+			this.isModified = isModified;
+			this.isFact = isFact;
+		}
+
+		public NormalRule getRule() {
+			return rule;
+		}
+
+		public boolean isModified() {
+			return isModified;
+		}
+
+		public boolean isFact() {
+			return isFact;
+		}
 	}
 
 	/**
@@ -92,69 +117,53 @@ public class SimplePreprocessing extends ProgramTransformation<NormalProgram, No
 	}
 
 	/**
-	 * This method checks rules for bodies containing non-derivable literals or negated literals, that are facts.
-	 * implements s9
+	 * This method checks for literals from rule bodies, that are already facts or non-derivable.
+	 * implements s9 + s10
 	 * @param rule the rule to check
 	 * @param rules the rules from which the literals should be derivable from
 	 * @param facts the facts from which the literals should be derivable from
-	 * @return true if the rule is non-derivable, false if it is derivable
+	 * @return SimpleReturn containing the (possibly modified) rule, a boolean isModified and a boolean isFact. On
+	 * non-derivable rules it returns the value NO_FIRE.
 	 */
-	private boolean checkForNonDerivableLiterals(NormalRule rule, List<NormalRule> rules, Set<Atom> facts) {
+	private SimpleReturn simplifyRule(NormalRule rule, List<NormalRule> rules, Set<Atom> facts) {
+		Set<Literal> redundantLiterals = new LinkedHashSet<>();
 		for (Literal literal : rule.getBody()) {
 			if (literal.isNegated()) {
 				if (facts.contains(literal.getAtom())) {
-					return true;
+					return SimpleReturn.NO_FIRE;
+				}
+				if (isNonDerivable(literal.getAtom(), rules, facts)) {
+					redundantLiterals.add(literal);
 				}
 			} else {
-				if (isNonDerivable(literal, rules)) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	/**
-	 * This method checks for literals from rule bodies, that are already facts or non-derivable.
-	 * implements s10
-	 * @param rule the rule to check
-	 * @param rules the rules from which the literals should be derivable from
-	 * @param facts the facts from which the literals should be derivable from
-	 * @return the possibly shortened rule or null, if the rule is not derivable
-	 */
-	private NormalRule simplifyRule(NormalRule rule, List<NormalRule> rules, Set<Atom> facts) {
-		for (Literal literal : rule.getBody()) {
-			if (literal.isNegated()) {
 				if (facts.contains(literal.getAtom())) {
-					return null;
-				}
-				else if (isNonDerivable(literal, rules)) {
-					return simplifyRule(removeLiteralFromRule(rule, literal), rules, facts);
-				}
-			}
-			else {
-				if (isNonDerivable(literal, rules)) {
-					return null;
-				}
-				else if (facts.contains(literal.getAtom())) {
-					return simplifyRule(removeLiteralFromRule(rule, literal), rules, facts);
+					if (literal.isGround()) {	//TODO: Safety Check
+						redundantLiterals.add(literal);
+					}
+				} else if (isNonDerivable(literal.getAtom(), rules, facts)) {
+					return SimpleReturn.NO_FIRE;
 				}
 			}
 		}
-		return rule;
+		if (redundantLiterals.isEmpty()) {
+			return new SimpleReturn(rule, false, false);
+		} else {
+			return removeLiteralsFromRule(rule, redundantLiterals);
+		}
 	}
 
 	/**
-	 * This method checks whether a literal is non-derivable, i.e. it is not unifiable with the head atom of a rule.
+	 * This method checks whether an atom is non-derivable, i.e. it is not unifiable with the head atom of a rule.
 	 * implements s5 conditions
-	 * @param literal the literal to check
+	 * @param atom the atom to check
 	 * @param rules the rules from which the literal should be derivable from
 	 * @return true if the literal is not derivable, false otherwise
 	 */
-	private boolean isNonDerivable(Literal literal, List<NormalRule> rules) {
+	private boolean isNonDerivable(Atom atom, List<NormalRule> rules, Set<Atom> facts) {
+		boolean unclear = false;
 		for (NormalRule rule : rules) {
 			boolean hasSharedVariable = false;
-			Set<VariableTerm> leftOccurringVariables = literal.getAtom().getOccurringVariables();
+			Set<VariableTerm> leftOccurringVariables = atom.getOccurringVariables();
 			Set<VariableTerm> rightOccurringVariables = rule.getHeadAtom().getOccurringVariables();
 			boolean leftSmaller = leftOccurringVariables.size() < rightOccurringVariables.size();
 			Set<VariableTerm> smallerSet = leftSmaller ? leftOccurringVariables : rightOccurringVariables;
@@ -162,30 +171,52 @@ public class SimplePreprocessing extends ProgramTransformation<NormalProgram, No
 			for (VariableTerm variableTerm : smallerSet) {
 				if (largerSet.contains(variableTerm)) {
 					hasSharedVariable = true;
+					unclear = true;
 					break;
 				}
 			}
 			if (!hasSharedVariable) {
-				if(Unification.unifyAtoms(literal.getAtom(), rule.getHeadAtom()) != null) {
+				if (Unification.instantiate(rule.getHeadAtom(), atom) != null) {
 					return false;
 				}
 			}
 		}
-		return true;
+		for (Atom fact : facts) {
+			boolean hasSharedVariable = false;
+			Set<VariableTerm> leftOccurringVariables = atom.getOccurringVariables();
+			Set<VariableTerm> rightOccurringVariables = fact.getOccurringVariables();
+			boolean leftSmaller = leftOccurringVariables.size() < rightOccurringVariables.size();
+			Set<VariableTerm> smallerSet = leftSmaller ? leftOccurringVariables : rightOccurringVariables;
+			Set<VariableTerm> largerSet = leftSmaller ? rightOccurringVariables : leftOccurringVariables;
+			for (VariableTerm variableTerm : smallerSet) {
+				if (largerSet.contains(variableTerm)) {
+					hasSharedVariable = true;
+					unclear = true;
+					break;
+				}
+			}
+			if (!hasSharedVariable) {
+				if (Unification.instantiate(fact, atom) != null) {
+					return false;
+				}
+			}
+		}
+		return !unclear;
 	}
 
 	/**
-	 * Returns a copy of the rule with the specified literal removed
-	 ** @return the rule without the specified literal
+	 * Returns a copy of the rule with the specified literals removed
+	 ** @return the rule without the specified literals
 	 */
-	private NormalRule removeLiteralFromRule(NormalRule rule, Literal literal) {
+	private SimpleReturn removeLiteralsFromRule(NormalRule rule, Set<Literal> literals) {
 		BasicAtom headAtom = rule.getHeadAtom();
-		List<Literal> newBody = new LinkedList<>();
+		Set<Literal> newBody = new LinkedHashSet<>();
 		for (Literal bodyLiteral : rule.getBody()) {
-			if (!literal.equals(bodyLiteral)) {
+			if (!literals.contains(bodyLiteral)) {
 				newBody.add(bodyLiteral);
 			}
 		}
-		return new NormalRuleImpl(Heads.newNormalHead(headAtom), newBody);
+		return new SimpleReturn(new NormalRuleImpl(Heads.newNormalHead(headAtom), new LinkedList<>(newBody)),
+				!literals.isEmpty(), newBody.isEmpty());
 	}
 }
