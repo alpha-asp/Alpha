891d1fbc5275f48e2d16628f519fdcb6e7b4315c
Michael Langowski <madmike200590@users.noreply.github.com>
2022-07-26 14:18:25 +0200

    Update alpha-api/src/main/java/at/ac/tuwien/kr/alpha/api/terms/IntervalTerm.java
    Co-authored-by: Antonius Weinzierl <AntoniusW@users.noreply.github.com>

--- alpha-api/src/main/java/at/ac/tuwien/kr/alpha/api/terms/IntervalTerm.java
+++ alpha-api/src/main/java/at/ac/tuwien/kr/alpha/api/terms/IntervalTerm.java
@@ -1,7 +1,7 @@
 package at.ac.tuwien.kr.alpha.api.terms;
 
 /**
- * A term representing an interval within the integers.
+ * A term representing an interval of integers.
  * Intervals are syntactic sugar, e.g. the rule "p(X) :- X = 1..2." is a shorthand for
  * "p(X) :- X = 1. p(X) :- X = 2.".
  */
