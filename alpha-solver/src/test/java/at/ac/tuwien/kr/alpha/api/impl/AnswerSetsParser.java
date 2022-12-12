c4ce84a02d6f94db26db923abee9e56f6e121126
Michael Langowski <e1426581@student.tuwien.ac.at>
2022-12-12 20:09:29 +0100

    parsing method for atom sets for use in testing

--- alpha-solver/src/test/java/at/ac/tuwien/kr/alpha/api/impl/AnswerSetsParser.java
+++ alpha-solver/src/test/java/at/ac/tuwien/kr/alpha/api/impl/AnswerSetsParser.java
@@ -2,6 +2,8 @@ package at.ac.tuwien.kr.alpha.api.impl;
 
 import java.util.Collections;
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import org.antlr.v4.runtime.BailErrorStrategy;
 import org.antlr.v4.runtime.CharStream;
@@ -12,10 +14,13 @@ import org.antlr.v4.runtime.atn.PredictionMode;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
 
 import at.ac.tuwien.kr.alpha.api.AnswerSet;
+import at.ac.tuwien.kr.alpha.api.programs.atoms.Atom;
+import at.ac.tuwien.kr.alpha.api.programs.atoms.BasicAtom;
 import at.ac.tuwien.kr.alpha.core.antlr.ASPCore2Lexer;
 import at.ac.tuwien.kr.alpha.core.antlr.ASPCore2Parser;
 import at.ac.tuwien.kr.alpha.core.parser.ParseTreeVisitor;
 
+// TODO this class should be called something different since it's turning into a general "atom set snippet parser"
 // TODO this is duplicated from core module, need to pull out test utils into separate testsupport module
 public class AnswerSetsParser {
 	
@@ -41,5 +46,32 @@ public class AnswerSetsParser {
 		parser.setErrorHandler(new BailErrorStrategy());
 
 		return VISITOR.translate(parser.answer_sets());
+	}
+
+	public static Set<BasicAtom> parseAtoms(String s) {
+		Set<AnswerSet> tmp = parse(s);
+		if(tmp.size() > 1) {
+			throw new IllegalArgumentException("Cannot parse multiset of atoms! Use answer set parsing methods instead!");
+		}
+		if(tmp.isEmpty()) {
+			return Collections.emptySet();
+		}
+		AnswerSet as = tmp.stream().findFirst().get();
+		Set<BasicAtom> atoms = as.getPredicates()
+			.stream()
+				.map(
+						(pred) -> as.getPredicateInstances(pred).stream()
+							.map((atom) -> {
+								if(!(atom instanceof BasicAtom)) {
+									throw new IllegalArgumentException("Expected a BasicAtom, but got: " + atom);
+								}
+								return (BasicAtom) atom;
+							}).collect(Collectors.toSet())
+				)
+				.reduce(new TreeSet<BasicAtom>(), (result, element) -> {
+					result.addAll(element);
+					return result;
+				});
+		return atoms;
 	}
 }
