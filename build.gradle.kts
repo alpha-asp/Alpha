2f34193a69d19cbb47679ba9591c4c80871452ac
Antonius Weinzierl <antonius.weinzierl@gmail.com>
2023-01-21 01:43:21 +0100

    Add Jacoco aggregation for coverage.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,26 @@
 plugins {
 	jacoco
-
 	id("com.github.kt3k.coveralls") version "2.12.0"
+	id("jacoco-report-aggregation")
 }
 
 tasks.wrapper {
 	gradleVersion = "7.6"
 	distributionType = Wrapper.DistributionType.ALL
+}
+
+dependencies {
+	jacocoAggregation(project(":alpha-cli-app"))
+}
+
+reporting {
+	reports {
+		val jacocoAggregatedTestReport by creating(JacocoCoverageReport::class) {
+			testType.set(TestSuiteType.UNIT_TEST)
+		}
+	}
+}
+
+repositories {
+	mavenCentral { metadataSources { mavenPom() } }
 }
