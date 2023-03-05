75ab2c477a6ab2bdacab49b92025f17391e7e711
Michael Langowski <michael.langowski@gmx.at>
2023-03-05 16:26:55 +0100

    add release plugin to build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import net.researchgate.release.ReleaseExtension
+
 plugins {
 	jacoco
 	id("com.github.kt3k.coveralls") version "2.12.0"
 	id("jacoco-report-aggregation")
+	id("net.researchgate.release") version "3.0.2"
 }
 
 tasks.wrapper {
@@ -22,5 +25,15 @@ reporting {
 }
 
 repositories {
+	maven ("https://plugins.gradle.org/m2/")
 	mavenCentral { metadataSources { mavenPom() } }
+}
+
+configure<ReleaseExtension> {
+	ignoredSnapshotDependencies.set(listOf("net.researchgate:gradle-release"))
+	failOnUnversionedFiles.set(false)
+	with(git) {
+		requireBranch.set("revamp-release-workflow")
+		// to disable branch verification: requireBranch.set(null as String?)
+	}
 }
